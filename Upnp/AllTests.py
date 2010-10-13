fc9fc6970212bebffb0f112db8e625f958b62dfb
Simon Chisholm <simon.chisholm@linn.co.uk>
2010-10-13 11:30:01 +0100

    Add a couple of command line options to AllTests.py.  -b builds only (i.e. tests are built but not executed).  -i performs an incremental rebuild (i.e. no make clean).

--- Upnp/AllTests.py
+++ Upnp/AllTests.py
@@ -34,41 +34,54 @@ def testNoArgs(aTarget):
     test(aTarget, [''])
 
 def runTests():
+    buildOnly = 0
+    incremental = 0
+    for arg in sys.argv:
+        if arg == '-b' or arg == '--buildonly':
+            buildOnly = 1
+        elif arg == '-i' or arg == '--incremental':
+            incremental = 1
+        elif arg != 'AllTests.py':
+            print 'Unrecognised argument - ' + arg
+            sys.exit(1)
+    if incremental == 0:
+        if os.name == 'nt':
+            cleanCmd = 'nmake /s /f Zapp.mak clean'
+        else:
+            cleanCmd = 'make clean'
+        os.system(cleanCmd)
     build('Tests')
     build('Zapp')
     build('CpProxyDlls')
     build('DvDeviceDlls')
-    testNoArgs('TestBuffer')
-    testNoArgs('TestThread')
-    testNoArgs('TestFifo')
-    testNoArgs('TestQueue')
-    testNoArgs('TestNetwork')
-    # TestEcho is interactive so can't be run here
-    # TestTimer is relatively long running and covers an area that rarely changes
-    test('TestSsdpMListen', ['-d', '10'])
-    test('TestSsdpUListen', ['-t', 'linn.co.uk:service:Radio:1'])
-    test('TestDeviceList', ['-t', 'linn.co.uk:service:Radio:1', '-f'])
-    test('TestDeviceListStd', ['-t', 'linn.co.uk:service:Radio:1', '-f'])
-    test('TestDeviceListC', ['-t', 'linn.co.uk:service:Radio:1', '-f'])
-    testNoArgs('TestInvocation')
-    testNoArgs('TestInvocationStd')
-    testNoArgs('TestSubscription')
-    testNoArgs('TestProxyC')
-    testNoArgs('TestDviDiscovery')
-    testNoArgs('TestDviDeviceList')
-    testNoArgs('TestDvInvocation')
-    testNoArgs('TestDvSubscription')
-    testNoArgs('TestDvDeviceStd')
-    testNoArgs('TestDvDeviceC')
-    # TestTopology1, TestTopology2 not quite ready for general use just yet
-    testNoArgs('TestProxyCs')
+    print '\nBuilds complete'
+    if buildOnly == 0:
+        testNoArgs('TestBuffer')
+        testNoArgs('TestThread')
+        testNoArgs('TestFifo')
+        testNoArgs('TestQueue')
+        testNoArgs('TestNetwork')
+        # TestEcho is interactive so can't be run here
+        # TestTimer is relatively long running and covers an area that rarely changes
+        test('TestSsdpMListen', ['-d', '10'])
+        test('TestSsdpUListen', ['-t', 'linn.co.uk:service:Radio:1'])
+        test('TestDeviceList', ['-t', 'linn.co.uk:service:Radio:1', '-f'])
+        test('TestDeviceListStd', ['-t', 'linn.co.uk:service:Radio:1', '-f'])
+        test('TestDeviceListC', ['-t', 'linn.co.uk:service:Radio:1', '-f'])
+        testNoArgs('TestInvocation')
+        testNoArgs('TestInvocationStd')
+        testNoArgs('TestSubscription')
+        testNoArgs('TestProxyC')
+        testNoArgs('TestDviDiscovery')
+        testNoArgs('TestDviDeviceList')
+        testNoArgs('TestDvInvocation')
+        testNoArgs('TestDvSubscription')
+        testNoArgs('TestDvDeviceStd')
+        testNoArgs('TestDvDeviceC')
+        # TestTopology1, TestTopology2 not quite ready for general use just yet
+        testNoArgs('TestProxyCs')
+        print '\nFinished.  All tests passed'
 
 cleanCmd = ''
-if os.name == 'nt':
-    cleanCmd = 'nmake /s /f Zapp.mak clean'
-else:
-    cleanCmd = 'make clean'
-os.system(cleanCmd)
 os.environ["ABORT_ON_FAILURE"] = "1"
 runTests()
-print '\nFinished.  All tests passed'
