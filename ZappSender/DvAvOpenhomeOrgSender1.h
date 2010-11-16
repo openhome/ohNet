7fd2a9f17a34f0f9b40a0ea060495e741836f320
grahamd <graham.darnell@linn.co.uk>
2010-11-16 12:00:18 +0000

    WavSender openhome harmonisation

--- ZappSender/DvAvOpenhomeOrgSender1.h
+++ ZappSender/DvAvOpenhomeOrgSender1.h
@@ -1,10 +1,10 @@
-#ifndef HEADER_DVMUSICOPENHOMEORGSENDER1
-#define HEADER_DVMUSICOPENHOMEORGSENDER1
+#ifndef HEADER_DVAVOPENHOMEORGSENDER1
+#define HEADER_DVAVOPENHOMEORGSENDER1
 
 #include <ZappTypes.h>
 #include <Buffer.h>
 #include <Core/DvDevice.h>
-#include <DvService.h>
+#include <DvProvider.h>
 
 namespace Zapp {
 
@@ -15,38 +15,44 @@ class PropertyString;
 class PropertyBinary;
 
 /**
- * Base Device for music.openhome.org:Sender:1
+ * Base Device for av.openhome.org:Sender:1
  */
-class DvServiceMusicOpenhomeOrgSender1 : public DvService
+class DvProviderAvOpenhomeOrgSender1 : public DvProvider
 {
 public:
-    virtual ~DvServiceMusicOpenhomeOrgSender1() {}
-    void SetPropertyMetadata(const Brx& aValue);
+    virtual ~DvProviderAvOpenhomeOrgSender1() {}
+    TBool SetPropertyPresentationUrl(const Brx& aValue);
+    void GetPropertyPresentationUrl(Brhz& aValue);
+    TBool SetPropertyMetadata(const Brx& aValue);
     void GetPropertyMetadata(Brhz& aValue);
-    void SetPropertyAudio(TBool aValue);
+    TBool SetPropertyAudio(TBool aValue);
     void GetPropertyAudio(TBool& aValue);
-    void SetPropertyStatus(const Brx& aValue);
+    TBool SetPropertyStatus(const Brx& aValue);
     void GetPropertyStatus(Brhz& aValue);
-    void SetPropertyAttributes(const Brx& aValue);
+    TBool SetPropertyAttributes(const Brx& aValue);
     void GetPropertyAttributes(Brhz& aValue);
 protected:
-    DvServiceMusicOpenhomeOrgSender1(DvDevice& aDevice);
+    DvProviderAvOpenhomeOrgSender1(DvDevice& aDevice);
+    void EnableActionPresentationUrl();
     void EnableActionMetadata();
     void EnableActionAudio();
     void EnableActionStatus();
     void EnableActionAttributes();
 private:
+    virtual void PresentationUrl(IInvocationResponse& aResponse, TUint aVersion, IInvocationResponseString& aValue);
     virtual void Metadata(IInvocationResponse& aResponse, TUint aVersion, IInvocationResponseString& aValue);
     virtual void Audio(IInvocationResponse& aResponse, TUint aVersion, IInvocationResponseBool& aValue);
     virtual void Status(IInvocationResponse& aResponse, TUint aVersion, IInvocationResponseString& aValue);
     virtual void Attributes(IInvocationResponse& aResponse, TUint aVersion, IInvocationResponseString& aValue);
 private:
-    DvServiceMusicOpenhomeOrgSender1();
+    DvProviderAvOpenhomeOrgSender1();
+    void DoPresentationUrl(IDvInvocation& aInvocation, TUint aVersion);
     void DoMetadata(IDvInvocation& aInvocation, TUint aVersion);
     void DoAudio(IDvInvocation& aInvocation, TUint aVersion);
     void DoStatus(IDvInvocation& aInvocation, TUint aVersion);
     void DoAttributes(IDvInvocation& aInvocation, TUint aVersion);
 private:
+    PropertyString* iPropertyPresentationUrl;
     PropertyString* iPropertyMetadata;
     PropertyBool* iPropertyAudio;
     PropertyString* iPropertyStatus;
@@ -55,5 +61,5 @@ private:
 
 } // namespace Zapp
 
-#endif // HEADER_DVMUSICOPENHOMEORGSENDER1
+#endif // HEADER_DVAVOPENHOMEORGSENDER1
 
