bbc2dc8628ee3365fa36a9bfad7942cb4ec195a0
Gregg Hamilton <gregg.hamilton@linn.co.uk>
2011-08-22 08:46:15 +0100

    Updated CpDeviceList classes in Java bindings to require only a single list listener constructor parameter.

--- OpenHome/Net/Bindings/Java/org/openhome/net/controlpoint/CpDeviceListUpnpUuid.java
+++ OpenHome/Net/Bindings/Java/org/openhome/net/controlpoint/CpDeviceListUpnpUuid.java
@@ -18,22 +18,17 @@ public class CpDeviceListUpnpUuid extends CpDeviceList
 	 * Constructor.
 	 * 
 	 * @param aUuid		unique identifier for the target device.	
-	 * @param aAdded	delegate which will be run when a new device is detected.
-	 * 					Clients who are interested this new device should call
-	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection.
-	 * 					This callback will never be run for a device that is already in the list.
-	 * @param aRemoved	delegate which will be run when a device is removed from the network.
-	 * 					Clients who had previously stored a reference to the device
-	 * 					in their <tt>aAdded</tt> callback should call
-	 * 					<tt>CpDevice.removeRef()</tt> and remove the device from their local collection.
+	 * @param aListener	delegate which will be called when a device is detected or removed.
+	 * 					Clients who are interested in a newly detected device should call
+	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection
+	 *					and must later call <tt>CpDevice.removeRef()</tt> to remove the
+	 *					device from their local collection.
 	 * 					Clients who had not previously claimed a reference to a device must
 	 * 					not call <tt>CpDevice.removeRef()</tt>.
 	 */
-	public CpDeviceListUpnpUuid(String aUuid, ICpDeviceListListener aAdded,
-			ICpDeviceListListener aRemoved)
+	public CpDeviceListUpnpUuid(String aUuid, ICpDeviceListListener aListener)
 	{
-		iAdded = aAdded;
-		iRemoved = aRemoved;
+		iListener = aListener;
 		CpDeviceListInitialised init = CpDeviceListCreateUpnpUuid(aUuid);
 		iHandle = init.getHandle();
 		iCallback = init.getCallback();
--- OpenHome/Net/Bindings/Java/org/openhome/net/controlpoint/CpUpnpDeviceListFactory.java
+++ OpenHome/Net/Bindings/Java/org/openhome/net/controlpoint/CpUpnpDeviceListFactory.java
@@ -23,65 +23,53 @@ public class CpUpnpDeviceListFactory implements ICpUpnpDeviceListFactory
 	/**
 	 * Create a device list that detects all UPnP devices.
 	 * 
-	 * @param aAdded	delegate which will be run when a new device is detected.
-	 * 					Clients who are interested this new device should call
-	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection.
-	 * 					This callback will never be run for a device that is already in the list.
-	 * @param aRemoved	delegate which will be run when a device is removed from the network.
-	 * 					Clients who had previously stored a reference to the device
-	 * 					in their <tt>aAdded</tt> callback should call
-	 * 					<tt>CpDevice.removeRef()</tt> and remove the device from their local collection.
+	 * @param aListener	delegate which will be called when a device is detected or removed.
+	 * 					Clients who are interested in a newly detected device should call
+	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection
+	 *					and must later call <tt>CpDevice.removeRef()</tt> to remove the
+	 *					device from their local collection.
 	 * 					Clients who had not previously claimed a reference to a device must
 	 * 					not call <tt>CpDevice.removeRef()</tt>.
 	 * @return			handle to an underlying native device list.
 	 */
-	public ICpDeviceList createListAll(ICpDeviceListListener aAdded,
-			ICpDeviceListListener aRemoved)
+	public ICpDeviceList createListAll(ICpDeviceListListener aListener)
 	{
-		return new CpDeviceListUpnpAll(aAdded, aRemoved);
+		return new CpDeviceListUpnpAll(aListener);
 	}
 
 	/**
 	 * Create a device list that detects all UPnP root devices.
 	 * 
-	 * @param aAdded	delegate which will be run when a new device is detected.
-	 * 					Clients who are interested this new device should call
-	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection.
-	 * 					This callback will never be run for a device that is already in the list.
-	 * @param aRemoved	delegate which will be run when a device is removed from the network.
-	 * 					Clients who had previously stored a reference to the device
-	 * 					in their <tt>aAdded</tt> callback should call
-	 * 					<tt>CpDevice.removeRef()</tt> and remove the device from their local collection.
+	 * @param aListener	delegate which will be called when a device is detected or removed.
+	 * 					Clients who are interested in a newly detected device should call
+	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection
+	 *					and must later call <tt>CpDevice.removeRef()</tt> to remove the
+	 *					device from their local collection.
 	 * 					Clients who had not previously claimed a reference to a device must
 	 * 					not call <tt>CpDevice.removeRef()</tt>.
 	 * @return			handle to an underlying native device list.
 	 */
-	public ICpDeviceList createListRoot(ICpDeviceListListener aAdded,
-			ICpDeviceListListener aRemoved)
+	public ICpDeviceList createListRoot(ICpDeviceListListener aListener)
 	{
-		return new CpDeviceListUpnpRoot(aAdded, aRemoved);
+		return new CpDeviceListUpnpRoot(aListener);
 	}
 
 	/**
 	 * Create a device list that detects a UPnP devices with a specific Udn.
 	 * 
 	 * @param aUuid		unique identifier for the target device.	
-	 * @param aAdded	delegate which will be run when a new device is detected.
-	 * 					Clients who are interested this new device should call
-	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection.
-	 * 					This callback will never be run for a device that is already in the list.
-	 * @param aRemoved	delegate which will be run when a device is removed from the network.
-	 * 					Clients who had previously stored a reference to the device
-	 * 					in their <tt>aAdded</tt> callback should call
-	 * 					<tt>CpDevice.removeRef()</tt> and remove the device from their local collection.
+	 * @param aListener	delegate which will be called when a device is detected or removed.
+	 * 					Clients who are interested in a newly detected device should call
+	 *					<tt>CpDevice.addRef()</tt> on it and add it to some local collection
+	 *					and must later call <tt>CpDevice.removeRef()</tt> to remove the
+	 *					device from their local collection.
 	 * 					Clients who had not previously claimed a reference to a device must
 	 * 					not call <tt>CpDevice.removeRef()</tt>.
 	 * @return			handle to an underlying native device list.
 	 */
-	public ICpDeviceList createListUuid(String aUuid, ICpDeviceListListener aAdded,
-			ICpDeviceListListener aRemoved)
+	public ICpDeviceList createListUuid(String aUuid, ICpDeviceListListener aListener)
 	{
-		return new CpDeviceListUpnpUuid(aUuid, aAdded, aRemoved);
+		return new CpDeviceListUpnpUuid(aUuid, aListener);
 	}
 
 	/**
@@ -90,24 +78,20 @@ public class CpUpnpDeviceListFactory implements ICpUpnpDeviceListFactory
 	 * @param aDomainName	vendor domain for the target device.
 	 * @param aDeviceType	name of the target device.
 	 * @param aVersion		version number for the target device.
-	 * @param aAdded		delegate which will be run when a new device is detected.
-	 * 						Clients who are interested this new device should call
-	 *						<tt>CpDevice.addRef()</tt> on it and add it to some local collection.
-	 * 						This callback will never be run for a device that is already in the list.
-	 * @param aRemoved		delegate which will be run when a device is removed from the network.
-	 * 						Clients who had previously stored a reference to the device
-	 * 						in their <tt>aAdded</tt> callback should call
-	 * 						<tt>CpDevice.removeRef()</tt> and remove the device from their local collection.
+	 * @param aListener		delegate which will be called when a device is detected or removed.
+	 * 						Clients who are interested in a newly detected device should call
+	 *						<tt>CpDevice.addRef()</tt> on it and add it to some local collection
+	 *						and must later call <tt>CpDevice.removeRef()</tt> to remove the
+	 *						device from their local collection.
 	 * 						Clients who had not previously claimed a reference to a device must
 	 * 						not call <tt>CpDevice.removeRef()</tt>.
 	 * @return				handle to an underlying native device list.
 	 */
 	public ICpDeviceList createListDeviceType(String aDomainName, String aDeviceType,
-			int aVersion, ICpDeviceListListener aAdded,
-			ICpDeviceListListener aRemoved)
+			int aVersion, ICpDeviceListListener aListener)
 	{
 		return new CpDeviceListUpnpDeviceType(aDomainName, aDeviceType, aVersion,
-				aAdded, aRemoved);
+				aListener);
 	}
 
 	/**
@@ -116,22 +100,18 @@ public class CpUpnpDeviceListFactory implements ICpUpnpDeviceListFactory
 	 * @param aDomainName	vendor domain for the target service (e.g. <tt>upnp.org</tt>).
 	 * @param aServiceType	name of the target service.
 	 * @param aVersion		version number for the target service.
-	 * @param aAdded		delegate which will be run when a new device is detected.
-	 * 						Clients who are interested this new device should call
-	 *						<tt>CpDevice.addRef()</tt> on it and add it to some local collection.
-	 * 						This callback will never be run for a device that is already in the list.
-	 * @param aRemoved		delegate which will be run when a device is removed from the network.
-	 * 						Clients who had previously stored a reference to the device
-	 * 						in their <tt>aAdded</tt> callback should call
-	 * 						<tt>CpDevice.removeRef()</tt> and remove the device from their local collection.
+	 * @param aListener		delegate which will be called when a device is detected or removed.
+	 * 						Clients who are interested in a newly detected device should call
+	 *						<tt>CpDevice.addRef()</tt> on it and add it to some local collection
+	 *						and must later call <tt>CpDevice.removeRef()</tt> to remove the
+	 *						device from their local collection.
 	 * 						Clients who had not previously claimed a reference to a device must
 	 * 						not call <tt>CpDevice.removeRef()</tt>.
 	 * @return				handle to an underlying native device list.
 	 */
 	public ICpDeviceList createListServiceType(String aDomainName,
-			String aServiceType, int aVersion, ICpDeviceListListener aAdded,
-			ICpDeviceListListener aRemoved)
+			String aServiceType, int aVersion, ICpDeviceListListener aListener)
 	{
-		return new CpDeviceListUpnpServiceType(aDomainName, aServiceType, aVersion, aAdded, aRemoved);
+		return new CpDeviceListUpnpServiceType(aDomainName, aServiceType, aVersion, aListener);
 	}
 }
