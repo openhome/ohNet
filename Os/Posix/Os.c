15b6c21ccd993d3025b00040d4dd2cad170368f6
Simon Chisholm <simon.chisholm@linn.co.uk>
2011-10-27 14:15:01 +0100

    Fix #64 - cope with NULL network adapters

--- Os/Posix/Os.c
+++ Os/Posix/Os.c
@@ -823,7 +823,8 @@ int32_t OsNetworkListAdapters(OsNetworkAdapter** aAdapters, uint32_t aUseLoopbac
     if (aUseLoopback == 0) {
         iter = networkIf;
         while (iter != NULL) {
-            if (iter->ifa_addr->sa_family == AF_INET &&
+            if (iter->ifa_addr != NULL &&
+                iter->ifa_addr->sa_family == AF_INET &&
                 ((struct sockaddr_in*)iter->ifa_addr)->sin_addr.s_addr != loopbackAddr) {
                 includeLoopback = 0;
                 break;
@@ -836,7 +837,7 @@ int32_t OsNetworkListAdapters(OsNetworkAdapter** aAdapters, uint32_t aUseLoopbac
     OsNetworkAdapter* head = NULL;
     OsNetworkAdapter* tail = NULL;
     while (iter != NULL) {
-        if (iter->ifa_addr->sa_family != AF_INET ||
+        if (iteriter->ifa_addr == NULL || iter->ifa_addr->sa_family != AF_INET ||
             (includeLoopback == 0 && ((struct sockaddr_in*)iter->ifa_addr)->sin_addr.s_addr == loopbackAddr) ||
             (aUseLoopback == 1 && ((struct sockaddr_in*)iter->ifa_addr)->sin_addr.s_addr != loopbackAddr)) {
             iter = iter->ifa_next;
