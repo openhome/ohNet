545d0c2c4a11961fbe14d8af80126a8b1ce590b5
Philip Gaw <philip.gaw@linn.co.uk>
2011-12-12 16:17:23 +0000

    check env variables and if set override the cmd line args

--- newbuild.py
+++ newbuild.py
@@ -4,15 +4,38 @@ from optparse import OptionParser
 
 class JenkinsBuild():
 	def get_options(self):
+		env_platform = os.environ.get('PLATFORM')
+		env_nightly = os.environ.get('NIGHTLY')
+		env_release = os.environ.get('RELEASE')
+
 		parser = OptionParser()
 		parser.add_option("-p", "--platform", dest="platform",
 			help="linux-x86, linux-x64, windows-x86, windows-x64, arm")
 		parser.add_option("-n", "--nightly",
                   action="store_true", dest="nightly", default=False,
                   help="Perform a nightly build")
+		parser.add_option("-r", "--release",
+		  action="store_true", dest="release", default=False,
+		  help="publish release")
 
 		(self.options, self.args) = parser.parse_args()
+		
+		# check if env variables are set
+		# if they are, ignore what is on the command line.
 
+		if env_platform != None:
+			self.options.platform = env_platform
+		
+		if env_nightly != None:
+			 self.options.nightly = env_nightly
+
+		if env_release != None:
+			self.options.release = env_release
+
+		print env_platform
+		print env_nightly
+		print env_release
+		
 	def get_platform(self):
 		
 		platforms = { 
@@ -23,9 +46,6 @@ class JenkinsBuild():
 		'macos-x64': { 'os': 'macos', 'arch':'x86'},
 		'arm': { 'os': 'linux', 'arch': 'arm'},
 		 }
-
-		if self.options.platform == None:
-			self.options.platform = os.environ.get('PLATFORM')
 		
 		current_platform = self.options.platform
 
@@ -65,8 +85,6 @@ class JenkinsBuild():
 		if os_platform == 'windows' and arch == 'x86':
 			args.append('--js')
 			args.append('--java')
-		if os_platform == 'windows' or nightly == True:
-			args.append('--release')
 		if os_platform == 'linux' and arch == 'x86':
 			args.append('--java')
 		if nightly == True:
@@ -77,19 +95,18 @@ class JenkinsBuild():
 
 	def do_build(self):
 		nightly = self.options.nightly
-		os_platform = self.platform['os']
+		release = self.options.release
 
+		os_platform = self.platform['os']
 		build_args = self.build_args
 		platform_args = self.platform_args
-
+			
 		if platform_args == []:
 			platform_args.extend(build_args)
 
 		else:
 			platform_args.append('&&')
 			platform_args.extend(build_args)
-
-		print "building with %s on platform %s" %(platform_args, os_platform)
 
 		ret = subprocess.check_call(platform_args)
 		if ret != 0:
@@ -103,7 +120,7 @@ def main():
 	Build.set_platform_args()
 	Build.get_build_args()
 	Build.do_build()
-	
+
 if __name__ == "__main__":
 	main()
 
