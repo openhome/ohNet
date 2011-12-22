a98d7b70fb282d6e9672628d7635dd351a7e183f
Simon Chisholm <simon.chisholm@linn.co.uk>
2011-12-22 11:37:18 +0000

    Remove a subscription when it fails to connect to its client's event server.  This may risk some clients not realising they've stopped receiving updates but its better than all updates slowing to a crawl due to repeated connection timeouts from clients who disappear without unsubscribing.

--- OpenHome/Net/Device/DviSubscription.cpp
+++ OpenHome/Net/Device/DviSubscription.cpp
@@ -126,7 +126,12 @@ void DviSubscription::WriteChanges()
             delete writer;
         }
     }
-	catch(NetworkTimeout&) {}
+	catch(NetworkTimeout&) {
+        // we may block a publisher for a relatively long time failing to connect
+        // its reasonable to assume that later attempts are also likely to fail
+        // ...so its better if we don't keep blocking and instead remove the subscription
+        iService->RemoveSubscription(iSid);
+    }
 	catch(NetworkError&) {}
 	catch(HttpError&) {}
 	catch(WriterError&) {}
