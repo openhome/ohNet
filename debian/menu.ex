74d86f62d781ac33e39281eae3e0ca074b3ea306
Cédric Andrieu <ced@opensource.wolfsonmicro.com>
2012-07-05 15:11:44 +0100

    Debian packaging files

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(ohnet):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="ohnet" command="/usr/bin/ohnet"
---
+++ debian/ohnet.default.ex
@@ -0,0 +1,10 @@
+# Defaults for ohnet initscript
+# sourced by /etc/init.d/ohnet
+# installed at /etc/default/ohnet by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
