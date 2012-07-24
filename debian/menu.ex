3e257529dd8d15957e00fae0f89e1a7acf10c92f
Cédric Andrieu <ced@opensource.wolfsonmicro.com>
2012-07-24 14:19:54 +0100

    Update Debian package build

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ohnet):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ohnet" command="/usr/bin/ohnet"
--- debian/ohnet.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ohnet package
-#
-0 4	* * *	root	[ -x /usr/bin/ohnet_maintenance ] && /usr/bin/ohnet_maintenance
