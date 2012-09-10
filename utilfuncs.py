a05b203f9c45924cc05dd26c8f9b2e31157fc03c
John Hagemeister <John.Hagemeister@linn.co.uk>
2012-09-10 09:37:03 +0100

    preemptively fixed spelling mistake
    ensure consistent spelling of "Volkano" for least surprise
    even though no adverse effects observed yet in practice

--- utilfuncs.py
+++ utilfuncs.py
@@ -112,7 +112,7 @@ def get_platform_info(dest_platform):
         'Linux-ARM': dict(endian='LITTLE',   build_platform='linux2', ohnet_plat_dir='Posix'),
         'Windows-x86': dict(endian='LITTLE', build_platform='win32',  ohnet_plat_dir='Windows'),
         'Windows-x64': dict(endian='LITTLE', build_platform='win32',  ohnet_plat_dir='Windows'),
-        'Core': dict(endian='BIG',           build_platform='linux2', ohnet_plat_dir='Volcano2'),
+        'Core': dict(endian='BIG',           build_platform='linux2', ohnet_plat_dir='Volkano2'),
         'Mac-x86': dict(endian='LITTLE',     build_platform='darwin', ohnet_plat_dir='Mac-x86'),
         'Mac-x64': dict(endian='LITTLE',     build_platform='darwin', ohnet_plat_dir='Mac-x64'),
         'iOs-ARM': dict(endian='LITTLE',     build_platform='darwin', ohnet_plat_dir='Mac/arm'),
