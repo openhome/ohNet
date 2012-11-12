d4c87862aa617d5426b82624bacec641946bba81
Andrew Wilson <andrew.wilson@linn.co.uk>
2012-11-12 15:00:38 +0000

    Add Mac-x64 as allowed target for bundle.

--- bundle_binaries.py
+++ bundle_binaries.py
@@ -44,7 +44,9 @@ ALL_TARGETS = {
         BuildTarget("Linux",   "armhf", "Release"): BuildInfo(builddir="Build/Obj/Posix/Release"),
 
         BuildTarget("Mac",     "x86",   "Debug"):   BuildInfo(builddir="Build/Obj/Mac/Debug"),
+        BuildTarget("Mac",     "x64",   "Debug"):   BuildInfo(builddir="Build/Obj/Mac/Debug"),
         BuildTarget("Mac",     "x86",   "Release"): BuildInfo(builddir="Build/Obj/Mac/Release"),
+        BuildTarget("Mac",     "x64",   "Release"): BuildInfo(builddir="Build/Obj/Mac/Release"),
 
         BuildTarget("iOs",     "x86",   "Debug"):   BuildInfo(builddir="Build/Obj/Mac/arm/Debug"),
         BuildTarget("iOs",     "armv6", "Debug"):   BuildInfo(builddir="Build/Obj/Mac/arm/Debug"),
