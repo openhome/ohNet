6570369d1e1631a76545bd7938c0cc933a948e98
Tim Cussins <tim.cussins@linn.co.uk>
2013-01-29 10:53:28 +0000

    Simplify File.h. Add static Factory method.

--- OpenHome/File.h
+++ OpenHome/File.h
@@ -23,45 +23,23 @@ typedef enum {
     eFileReadWrite,
 } FileMode;
 
-class IFileSeekable
+class IFile 
 {
 public:
-    virtual void Seek(TInt32 aBytes, SeekWhence aWhence) = 0;
-    virtual TUint32 Tell() const = 0;
-    virtual TUint32 Bytes() = 0;
-};
 
-class IFileReadable
-{
-public:
+    static IFile* Open(const TChar* aFilename, FileMode aFileMode);
+
+    virtual ~IFile() {}
+
     virtual void Read(Bwx& aBuffer) = 0;
-    virtual void Read(Bwx& aBuffer, TUint32 aBytes) = 0;
-};
+    virtual void Read(Bwx& aBuffer, TUint aBytes) = 0;
 
-class IFileWriteable
-{
-public:
     virtual void Write(const Brx& aBuffer) = 0;
     virtual void Write(const Brx& aBuffer, TUint32 aBytes) = 0;
-};
 
-class File : public IFileSeekable, public IFileReadable, public IFileWriteable
-{
-public:
-    File(const TChar* aFilename, FileMode aFileMode);
-    virtual ~File();
-
-    virtual void Read(Bwx& aBuffer);
-    virtual void Read(Bwx& aBuffer, TUint aBytes);
-
-    virtual void Write(const Brx& aBuffer);
-    virtual void Write(const Brx& aBuffer, TUint32 aBytes);
-
-    virtual void Seek(TInt32 aBytes, SeekWhence aWhence = eSeekFromStart);
-    virtual TUint32 Tell() const;
-    virtual TUint32 Bytes();
-protected:
-    THandle iHandle;
+    virtual void Seek(TInt32 aBytes, SeekWhence aWhence = eSeekFromStart) = 0;
+    virtual TUint32 Tell() const = 0;
+    virtual TUint32 Bytes() = 0;
 };
 
 } // namespace OpenHome
