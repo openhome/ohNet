f995cd8aab81f187fadf2278b4b68c2be3597ddb
Simon Chisholm <simon.chisholm@linn.co.uk>
2013-02-04 12:10:38 +0000

    Fix for race that could lead to an event other than the initial event being processed first.  This meant that the initial event callback ran before all properties had their values set.

--- OpenHome/Net/ControlPoint/CpiSubscription.cpp
+++ OpenHome/Net/ControlPoint/CpiSubscription.cpp
@@ -54,11 +54,18 @@ void CpiSubscription::RemoveRef()
 
 TBool CpiSubscription::UpdateSequenceNumber(TUint aSequenceNumber)
 {
+    iLock.Wait();
     if (aSequenceNumber != iNextSequenceNumber) {
+        iLock.Signal();
         return false;
     }
     iNextSequenceNumber++;
     return true;
+}
+
+void CpiSubscription::Unlock()
+{
+    iLock.Signal();
 }
 
 void CpiSubscription::SetNotificationError()
@@ -311,7 +318,6 @@ void CpiSubscription::SetRenewTimer(TUint aMaxSeconds)
 
 void CpiSubscription::EventUpdateStart()
 {
-    iLock.Wait();
     if (iEventProcessor != NULL) {
         iEventProcessor->EventUpdateStart();
     }
@@ -329,7 +335,6 @@ void CpiSubscription::EventUpdateEnd()
     if (iEventProcessor != NULL) {
         iEventProcessor->EventUpdateEnd();
     }
-    iLock.Signal();
 }
 
 void CpiSubscription::ListObjectDetails() const
