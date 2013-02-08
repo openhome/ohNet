a451793e4e0dc7359bde9272b179c5c5deaa1229
Tim Cussins <tim.cussins@linn.co.uk>
2013-02-08 14:44:29 +0000

    CodecController should pass Quit messages downstream.

--- OpenHome/Media/Codec/CodecController.cpp
+++ OpenHome/Media/Codec/CodecController.cpp
@@ -274,6 +274,7 @@ Msg* CodecController::ProcessMsg(MsgFlush* aMsg)
 
 Msg* CodecController::ProcessMsg(MsgQuit* aMsg)
 {
+    Queue(aMsg);
     iQuit = true;
     iStreamEnded = true;
     return aMsg;
