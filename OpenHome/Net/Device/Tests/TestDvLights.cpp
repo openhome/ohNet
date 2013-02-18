de71af10b0a34b899e352bd54608ddf2f10dbacd
Tim Cussins <tim.cussins@linn.co.uk>
2013-02-18 09:49:39 +0000

    Adapt TestDvLights, TestDvTestBasic to the IFile interface.

--- OpenHome/Net/Device/Tests/TestDvLights.cpp
+++ OpenHome/Net/Device/Tests/TestDvLights.cpp
@@ -9,9 +9,9 @@
 #include <OpenHome/Private/Env.h>
 #include <OpenHome/MimeTypes.h>
 #include <OpenHome/Net/Private/Globals.h>
+#include <OpenHome/Private/File.h>
 
 #include <vector>
-#include <sys/stat.h>
 
 using namespace OpenHome;
 using namespace OpenHome::Net;
@@ -279,16 +279,18 @@ void DeviceLights::WriteResource(const Brx& aUriTail, TIpAddress /*aInterface*/,
     filePath.Append(file);
     filePath.PtrZ();
 
+    IFile* filePtr = NULL;
+
     const char* path = (const char*)filePath.Ptr();
-    FILE* fd = fopen(path, "rb");
-    if (fd == NULL) {
+    try {
+        filePtr = IFile::Open(path, eFileReadOnly);
+    }
+    catch ( FileOpenError ) {
         return;
     }
+
     static const TUint kMaxReadSize = 4096;
-    struct stat fileStats;
-    (void)stat(path, &fileStats);
-    TUint bytes = (TUint)fileStats.st_size;
-    //Print("File %s size is %u\n", path, bytes);
+    TUint bytes = filePtr->Bytes();
     const char* mime = NULL;
     for (TUint i=filePath.Bytes()-1; i>0; i--) {
         if (filePath[i] == '/' || filePath[i] == '\\') {
@@ -321,17 +323,15 @@ void DeviceLights::WriteResource(const Brx& aUriTail, TIpAddress /*aInterface*/,
     Print("selected mime %s\n", mime);
     aResourceWriter.WriteResourceBegin(bytes, mime);
     do {
-        TByte buf[kMaxReadSize];
+        Bws<kMaxReadSize> buf;
         TUint size = (bytes<kMaxReadSize? bytes : kMaxReadSize);
-        size_t records_read = fread(buf, size, 1, fd);
-        ASSERT(records_read == 1);
-
-        aResourceWriter.WriteResource(buf, size);
+        filePtr->Read(buf, size);
+        ASSERT(buf.Bytes() == size);
+        aResourceWriter.WriteResource(buf.Ptr(), buf.Bytes());
         bytes -= size;
-        //Print("Written %u, remaining %u\n", size, bytes);
     } while (bytes > 0);
     aResourceWriter.WriteResourceEnd();
-    (void)fclose(fd);
+    delete filePtr;
 }
 
 
