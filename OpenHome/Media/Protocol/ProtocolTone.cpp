a37a34ee9ecce4c4282ee5810349813f8b1f74dd
John Hagemeister <John.Hagemeister@linn.co.uk>
2013-05-29 14:35:39 +0100

    stricter parameter checking / clean-up
    return with failure code if any parameter missing;
    reduced visual clutter with new LOG_DBG macro;
    minor tweaks to conform with Linn coding conventions
    (function return types, unused formal parameters in stub functions)

--- OpenHome/Media/Protocol/ProtocolTone.cpp
+++ OpenHome/Media/Protocol/ProtocolTone.cpp
@@ -8,6 +8,12 @@
 #include <OpenHome/Private/Printer.h>  // XXX Log::Print()
 #include <OpenHome/Media/Msg.h>  // XXX MsgFlush::kIdInvalid
 
+#ifndef NDEBUG
+    #define LOG_DBG(msg, val) \
+        Log::Print("!!  ProtocolTone::Stream(): "); Log::Print(msg); Log::Print(": "); Log::Print(val); Log::Print("\n");
+#else
+    #define LOG_DBG(msg, val) /* NOP */
+#endif
 
 //
 // declarations
@@ -53,47 +59,41 @@ ProtocolTone::ProtocolTone(Environment& aEnv) : Protocol(aEnv)
 // XXX any need to override virtual dtor?
 // ProtocolTone::~ProtocolTone() { }
 
-TUint
-ProtocolTone::TryStop(TUint aTrackId, TUint aStreamId)
+TUint ProtocolTone::TryStop(TUint /* aTrackId */, TUint /* aStreamId */)
 {
-    (void) aTrackId;  // XXX
-    (void) aStreamId;  // XXX
     return MsgFlush::kIdInvalid;  // XXX
 }
 
 // TUint ProtocolTone::TrySeek(TUint aTrackId, TUint aStreamId, TUint64 aOffset) { }
 
-ProtocolStreamResult
-ProtocolTone::Stream(const Brx& aUri)
+ProtocolStreamResult ProtocolTone::Stream(const Brx& aUri)
 {
     // tone://WAVEFORM.wav?bitdepth=N&samplerate=M&pitch=HZ&channels=K&duration=T
     Uri uri(aUri);
     if (uri.Scheme() != Brn("tone")) {
-        return EProtocolErrorNotSupported;  // XXX
+        // quietly decline offer to handle other URL schemes; leave it to other protocol modules
+        return EProtocolErrorNotSupported;
     }
-    Log::Print("@@  ProtocolTone::Stream(): scheme 'tone://'\n");  // XXX
+    LOG_DBG("scheme", uri.Scheme())
 
-    // Log::Print("@@  ProtocolTone::Stream(): host = ");  Log::Print(uri.Host());  Log::Print("\n");  // XXX
     const Brx& waveForm(uri.Host());
     if ((waveForm != Brn("square.wav")) && (waveForm != Brn("sawtooth.wav"))) {
-        Log::Print("!!  ProtocolTone::Stream(): host = ");  Log::Print(uri.Host());  Log::Print("\n");  // XXX
-        return EProtocolErrorNotSupported;  // XXX
+        LOG_DBG("unsupported waveform", uri.Host())
+        return EProtocolErrorNotSupported;
     }
 
-    // Log::Print("@@  ProtocolTone::Stream(): path = ");  Log::Print(uri.Path());  Log::Print("\n");  // XXX
     if (uri.Path() != Brn("/")) {
-        Log::Print("!!  ProtocolTone::Stream(): path = ");  Log::Print(uri.Path());  Log::Print("\n");  // XXX
-        return EProtocolStreamErrorUnrecoverable;  // XXX
+        LOG_DBG("spurious path", uri.Path())
+        return EProtocolStreamErrorUnrecoverable;
     }
 
     // XXX generalise query parsing into key/value string pairs?
     const Brx& query(uri.Query());
     if (!query.BeginsWith(Brn("?"))) {
-        Log::Print("!!  ProtocolTone::Stream(): query = ");  Log::Print(uri.Query());  Log::Print("\n");  // XXX
-        return EProtocolStreamErrorUnrecoverable;  // XXX
+        LOG_DBG("missing or malformed query", uri.Query())
+        return EProtocolStreamErrorUnrecoverable;
     }
 
-    // Log::Print("@@  ProtocolTone::Stream(): query = ");  Log::Print(uri.Query());  Log::Print("\n");  // XXX
     Parser parser(query);
     parser.Forward(1);  // initial "?" already checked
 
@@ -106,7 +106,6 @@ ProtocolTone::Stream(const Brx& aUri)
 
     while (!parser.Finished()) {
         const Brn& keyVal = parser.Next('&');
-        // Log::Print("@@  key=value: ");  Log::Print(keyVal);  Log::Print("\n");  // XXX
 
         Parser kvParser(keyVal);
         const Brn& key = kvParser.Next('=');
@@ -114,18 +113,18 @@ ProtocolTone::Stream(const Brx& aUri)
 
         if (key == Brn("bitdepth")) {
             if (bitdepth != 0) {
-                Log::Print("!!  ProtocolTone::Stream(): duplicate parameter: bitdepth\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("duplicate parameter", "bitdepth")
+                return EProtocolStreamErrorUnrecoverable;
             }
             bitdepth = Ascii::Int(val);
             if ((bitdepth != 8) && (bitdepth != 16) && (bitdepth != 24)) {
-                Log::Print("!!  ProtocolTone::Stream(): invalid parameter value: ");  Log::Print(keyVal);  Log::Print("\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("invalid parameter value", keyVal)
+                return EProtocolStreamErrorUnrecoverable;
             }
         } else if (key == Brn("samplerate")) {
             if (samplerate != 0) {
-                Log::Print("!!  ProtocolTone::Stream(): duplicate parameter: samplerate\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("duplicate parameter", "samplerate")
+                return EProtocolStreamErrorUnrecoverable;
             }
             samplerate = Ascii::Int(val);
             switch (samplerate) {
@@ -136,66 +135,71 @@ ProtocolTone::Stream(const Brx& aUri)
                 case 12000: case 24000: case 48000: case 96000: case 192000:
                     break;
                 default:
-                    Log::Print("!!  ProtocolTone::Stream(): invalid parameter value: ");  Log::Print(keyVal);  Log::Print("\n");  // XXX
-                    return EProtocolStreamErrorUnrecoverable;  // XXX
+                    LOG_DBG("invalid parameter value", keyVal)
+                    return EProtocolStreamErrorUnrecoverable;
             }
         } else if (key == Brn("pitch")) {  // [Hz]
             if (pitch != 0) {
-                Log::Print("!!  ProtocolTone::Stream(): duplicate parameter: pitch\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("duplicate parameter", "pitch")
+                return EProtocolStreamErrorUnrecoverable;
             }
             pitch = Ascii::Int(val);
             // XXX no upper limit, since not necessarily intended for human hearing
             if (! (0 < pitch)) {
-                Log::Print("!!  ProtocolTone::Stream(): invalid parameter value: ");  Log::Print(keyVal);  Log::Print("\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("invalid parameter value", keyVal)
+                return EProtocolStreamErrorUnrecoverable;
             }
         } else if (key == Brn("channels")) {
             if (channels != 0) {
-                Log::Print("!!  ProtocolTone::Stream(): duplicate parameter: channels\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("duplicate parameter", "channels")
+                return EProtocolStreamErrorUnrecoverable;
             }
             // 1 ... 8 (in practice no more than 7.1 surround sound)
             channels = Ascii::Int(val);
             if (! ((0 < channels) && (channels <= 8))) {
-                Log::Print("!!  ProtocolTone::Stream(): invalid parameter value: ");  Log::Print(keyVal);  Log::Print("\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("invalid parameter value", keyVal)
+                return EProtocolStreamErrorUnrecoverable;
             }
         } else if (key == Brn("duration")) {  // [s]
             if (duration != 0) {
-                Log::Print("!!  ProtocolTone::Stream(): duplicate parameter: duration\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("duplicate parameter", "duration")
+                return EProtocolStreamErrorUnrecoverable;
             }
             // 1 ... 900 (i.e. 15min): arbitrary limit guaranteed to avoid integer overflow in calculations
             duration = Ascii::Int(val);
             if (! ((0 < duration) && (duration <= 900))) {
-                Log::Print("!!  ProtocolTone::Stream(): invalid parameter value: ");  Log::Print(keyVal);  Log::Print("\n");  // XXX
-                return EProtocolStreamErrorUnrecoverable;  // XXX
+                LOG_DBG("invalid parameter value", keyVal)
+                return EProtocolStreamErrorUnrecoverable;
             }
         } else {
-            Log::Print("!!  ProtocolTone::Stream(): unrecognised keyword: ");  Log::Print(key);  Log::Print("\n");  // XXX
-            return EProtocolStreamErrorUnrecoverable;  // XXX
+            LOG_DBG("unrecognised keyword", key)
+            return EProtocolStreamErrorUnrecoverable;
         }
     }
 
     if (bitdepth == 0) {
-        Log::Print("!!  ProtocolTone::Stream(): missing parameter or illegal value: bitdepth\n");  // XXX
+        LOG_DBG("missing parameter", "bitdepth")
+        return EProtocolStreamErrorUnrecoverable;
     } else if (samplerate == 0) {
-        Log::Print("!!  ProtocolTone::Stream(): missing parameter or illegal value: samplerate\n");  // XXX
+        LOG_DBG("missing parameter", "samplerate")
+        return EProtocolStreamErrorUnrecoverable;
     } else if (pitch == 0) {
-        Log::Print("!!  ProtocolTone::Stream(): missing parameter or illegal value: pitch\n");  // XXX
+        LOG_DBG("missing parameter", "pitch")
+        return EProtocolStreamErrorUnrecoverable;
     } else if (channels == 0) {
-        Log::Print("!!  ProtocolTone::Stream(): missing parameter or illegal value: channels\n");  // XXX
+        LOG_DBG("missing parameter", "channels")
+        return EProtocolStreamErrorUnrecoverable;
     } else if (duration == 0) {
-        Log::Print("!!  ProtocolTone::Stream(): missing parameter or illegal value: duration\n");  // XXX
+        LOG_DBG("missing parameter", "duration")
+        return EProtocolStreamErrorUnrecoverable;
     }
 
-    Log::Print("@@  ProtocolTone::Stream(): successfully parsed all parameters\n");  // XXX
-    Log::Print("@@  bitdepth =   %6u\n", bitdepth);  // XXX
-    Log::Print("@@  samplerate = %6u\n", samplerate);  // XXX
-    Log::Print("@@  pitch =      %6u\n", pitch);  // XXX
-    Log::Print("@@  channels =   %6u\n", channels);  // XXX
-    Log::Print("@@  duration =   %6u\n", duration);  // XXX
+    LOG_DBG("successfully parsed all parameters", "")
+    Log::Print("@@  bitdepth =   %6u\n", bitdepth);
+    Log::Print("@@  samplerate = %6u\n", samplerate);
+    Log::Print("@@  pitch =      %6u\n", pitch);
+    Log::Print("@@  channels =   %6u\n", channels);
+    Log::Print("@@  duration =   %6u\n", duration);
 
-    return EProtocolErrorNotSupported;  // XXX
+    return EProtocolErrorNotSupported;
 }
