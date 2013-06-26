69e3145212d6f46b54e5eb594650bf4d7c661c4a
John Hagemeister <John.Hagemeister@linn.co.uk>
2013-06-26 12:32:45 +0100

    added initial draft implementation of MuteManager

---
+++ OpenHome/Media/MuteManager.cpp
@@ -0,0 +1,179 @@
+#include <OpenHome/Media/MuteManager.h>
+#include <OpenHome/Buffer.h>
+
+#include <OpenHome/Private/Thread.h>
+#include <OpenHome/Private/Standard.h>
+
+using namespace OpenHome;
+using namespace OpenHome::Media;
+
+//
+// interface
+//
+
+IMute::~IMute()
+{
+    // NOP default implementation (for convenience)
+}
+
+//
+// reference-counting base class
+//
+
+// XXX thread-safety for MuteCounted
+//
+// implicit assumption:  not in fact used directly by client, but always
+//     (a) part of collection (see below);  or
+//     (b) directly reg'd w/ MuteManager (and hence silently wrapped in MuteSafe)
+MuteCounted::MuteCounted(IMute& aMute)
+    : iMute(aMute)
+    , iCount(0)
+{
+}
+
+void MuteCounted::Mute()
+{
+    // XXX integer overflow (unlikely)
+    ++iCount;
+    if (1 == iCount) {
+        iMute.Mute();
+    }
+}
+
+void MuteCounted::Unmute()
+{
+    ASSERT(iCount > 0);  // integer underflow: programmer error
+    --iCount;
+    if (0 == iCount) {
+        iMute.Unmute();
+    }
+}
+
+//
+// collection
+//
+
+MuteCollection::MuteCollection()
+    : iMuteChain()
+{
+}
+
+//
+// XXX thread-safety for MuteCollection
+//
+// individual elements will NOT be wrapped in MuteSafe (hidden impl)
+// implicit assumption:  collection built up at start-up and never changed after first use
+// Add():  implicit assumption that each collection is built up by single thread?!
+// Mute()/Unmute():  implicit assumption that collection is reg'd (and wrapped) by MuteManager?!
+//     if so: wrapped collection will acquire lock and complete traversal w/o intervening ops
+void MuteCollection::Add(IMute& aMute)
+{
+    iMuteChain.push_back(&aMute);
+}
+
+void MuteCollection::Mute()
+{
+    for (TUint i = 0; i < iMuteChain.size(); ++i) {
+        iMuteChain[i]->Mute();
+    }
+}
+
+void MuteCollection::Unmute()
+{
+    for (TInt i = iMuteChain.size() - 1; i <= 0; --i) {
+        iMuteChain[i]->Unmute();
+    }
+}
+
+//
+// dummy mute element
+//
+
+void MuteNull::Mute()
+{
+    // NOP: dummy
+}
+
+void MuteNull::Unmute()
+{
+    // NOP: dummy
+}
+
+//
+// manager
+//
+
+namespace OpenHome {
+namespace Media {
+
+// (subsystem) concurrency control wrapper (implementation detail of MuteManager)
+class MuteSafe : public IMute
+{
+public:
+    MuteSafe(const TChar* aName, IMute& aMute, Mutex& aMutex);
+    const Brx& Name() const;
+
+public:  // from IMute
+    void Mute();
+    void Unmute();
+
+private:
+    Mutex& iMutex;  // non-recursive
+    IMute& iMute;
+    Bws<32> iName;
+};
+
+MuteSafe::MuteSafe(const TChar* aName, IMute& aMute, Mutex& aMutex)
+    : iMutex(aMutex)
+    , iMute(aMute)
+{
+    iName.Replace(aName);
+}
+
+const Brx& MuteSafe::Name() const
+{
+    return iName;
+}
+
+void MuteSafe::Mute()
+{
+    AutoMutex mutex(iMutex);
+    iMute.Mute();
+}
+
+void MuteSafe::Unmute()
+{
+    AutoMutex mutex(iMutex);
+    iMute.Unmute();
+}
+
+} // namespace Media
+} // namespace OpenHome
+
+// concrete manager
+MuteManager::MuteManager()
+    : iMuteCollection()
+    , iMutex("MUTE")
+{
+}
+
+// XXX implicit assumption:  aMute not already wrapped in MuteSafe
+void MuteManager::Add(const TChar* aName, IMute& aMute)
+{
+    AutoMutex mutex(iMutex);
+    // clients expected to invoke this only at start-up time,
+    // so no concerns over dynamic memory allocation
+    iMuteCollection.push_back(new MuteSafe(aName, aMute, iMutex));
+}
+
+IMute& MuteManager::Find(const TChar* aName) const
+{
+    AutoMutex mutex(iMutex);
+    for (TUint i = 0; i < iMuteCollection.size(); ++i) {
+        if (iMuteCollection[i]->Name() == Brn(aName)) {
+            // do not unwrap, since implicit locking desired
+            return *iMuteCollection[i];
+        }
+    }
+    THROW(MuteManagerError);  // programmer error
+}
---
+++ OpenHome/Media/MuteManager.h
@@ -0,0 +1,94 @@
+#ifndef HEADER_MUTE_MANAGER
+#define HEADER_MUTE_MANAGER
+
+#include <OpenHome/OhNetTypes.h>
+
+#include <OpenHome/Private/Thread.h>  // Mutex
+
+#include <vector>
+
+EXCEPTION(MuteManagerError);
+
+namespace OpenHome {
+namespace Media {
+
+// element interface
+class IMute
+{
+public:
+    virtual ~IMute();
+    virtual void Mute() = 0;
+    virtual void Unmute() = 0;
+};
+
+// reference-counting base class (does not own wrapped IMute element)
+class MuteCounted : public IMute, public INonCopyable
+{
+public:
+    MuteCounted(IMute& aMute);
+
+public:  // from IMute
+    void Mute();
+    void Unmute();
+
+private:
+    IMute& iMute;
+    TUint iCount;
+};
+
+// collection does not own individual IMute elements
+class MuteCollection : public IMute
+{
+public:
+    MuteCollection();
+    void Add(IMute& aMute);
+
+public:  // from IMute
+    void Mute();
+    void Unmute();
+
+private:
+    std::vector<IMute*> iMuteChain;
+};
+
+// dummy mute element
+// generic code may expect lookup of particular mute by name to succeed
+// even when hardware does not actually provide capability
+class MuteNull : public IMute
+{
+public:  // from IMute
+    void Mute();
+    void Unmute();
+};
+
+// manager interface
+class IMuteManager
+{
+public:
+    virtual IMute& Find(const TChar* aName) const = 0;
+};
+
+// concrete manager (for loose coupling in generic code)
+// does not own individual IMute elements
+class MuteSafe;
+
+class MuteManager : public IMuteManager
+{
+public:
+    MuteManager();
+    // only directly constructed objects must be registered,
+    // i.e. do not Add() anything returned by Find()
+    void Add(const TChar* aName, IMute& aMute);
+public:  // from IMuteManager
+    // throws MuteManagerError if not found
+    IMute& Find(const TChar* aName) const;
+
+private:
+    std::vector<MuteSafe*> iMuteCollection;
+    mutable Mutex iMutex;
+};
+
+} // namespace Media
+} // namespace OpenHome
+
+#endif // HEADER_MUTE_MANAGER
