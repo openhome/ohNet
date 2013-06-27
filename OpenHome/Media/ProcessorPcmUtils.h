04200bbfb10e19ca0e5863186f0e3111e981f3bd
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2013-06-27 16:16:59 +0100

    Moved ProcessorPcmBufPackedDualMono into DriverSongcastSender

--- OpenHome/Media/ProcessorPcmUtils.h
+++ OpenHome/Media/ProcessorPcmUtils.h
@@ -51,20 +51,6 @@ private: // from IPcmProcessor
     void ProcessSample24(const TByte* aSample, TUint aNumChannels);
 };
 
-class ProcessorPcmBufPackedDualMono : public ProcessorPcmBuf
-{
-public:
-    ProcessorPcmBufPackedDualMono();
-private: // from IPcmProcessor
-    TBool ProcessFragment8(const Brx& aData, TUint aNumChannels);
-    TBool ProcessFragment16(const Brx& aData, TUint aNumChannels);
-    TBool ProcessFragment24(const Brx& aData, TUint aNumChannels);
-    void ProcessSample8(const TByte* aSample, TUint aNumChannels);
-    void ProcessSample16(const TByte* aSample, TUint aNumChannels);
-    void ProcessSample24(const TByte* aSample, TUint aNumChannels);
-};
-
-
 } // namespace Media
 } // namespace OpenHome
 
