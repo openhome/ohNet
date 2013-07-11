728cdd85737ea18e32d37252e6e7d7630d0c89e8
Simon Chisholm <simon.chisholm@linn.co.uk>
2013-07-11 14:53:02 +0100

    Fix for resource leak in Java bindings

--- OpenHome/Net/Bindings/Java/Property.c
+++ OpenHome/Net/Bindings/Java/Property.c
@@ -14,74 +14,87 @@ extern "C" {
 
 void STDCALL ChangeCallback(void* aPtr)
 {
-	JniObjRef* ref = (JniObjRef*) aPtr;
-	JavaVM *vm = ref->vm;
+    JniObjRef* ref = (JniObjRef*) aPtr;
+    JavaVM *vm = ref->vm;
     JNIEnv *env;
-	jclass cls;
-	jmethodID mid;
-	jint ret;
-	jint attached;
+    jclass cls;
+    jmethodID mid;
+    jint ret;
+    jint attached;
 
-	attached = (*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_4);
-	if (attached < 0)
-	{
+    attached = (*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_4);
+    if (attached < 0)
+    {
 #ifdef __ANDROID__
-		ret = (*vm)->AttachCurrentThreadAsDaemon(vm, &env, NULL);
+        ret = (*vm)->AttachCurrentThreadAsDaemon(vm, &env, NULL);
 #else
-		ret = (*vm)->AttachCurrentThreadAsDaemon(vm, (void **)&env, NULL);
+        ret = (*vm)->AttachCurrentThreadAsDaemon(vm, (void **)&env, NULL);
 #endif
-		if (ret < 0)
-		{
-			printf("PropertyJNI: Unable to attach thread to JVM.\n");
-			fflush(stdout);
-			return;
-		}
-	}
-	cls = (*env)->GetObjectClass(env, ref->callbackObj);
-	mid = (*env)->GetMethodID(env, cls, "notifyChange", "()V");
+        if (ret < 0)
+        {
+            printf("PropertyJNI: Unable to attach thread to JVM.\n");
+            fflush(stdout);
+            return;
+        }
+    }
+    cls = (*env)->GetObjectClass(env, ref->callbackObj);
+    mid = (*env)->GetMethodID(env, cls, "notifyChange", "()V");
     (*env)->DeleteLocalRef(env, cls);
-	if (mid == 0) {
-		printf("PropertyJNI: Method ID notifyChange() not found.\n");
-		return;
-	}
-	(*env)->CallVoidMethod(env, ref->callbackObj, mid);
-	
-	// leave daemon thread attached to the VM
+    if (mid == 0) {
+        printf("PropertyJNI: Method ID notifyChange() not found.\n");
+        return;
+    }
+    (*env)->CallVoidMethod(env, ref->callbackObj, mid);
+    
+    // leave daemon thread attached to the VM
 }
 
 void STDCALL InitialiseReferences(JNIEnv *aEnv, jobject aObject, JniObjRef **aRef)
 {
-	jint ret;
+    jint ret;
 
-	*aRef = (JniObjRef*) malloc(sizeof(JniObjRef));
+    *aRef = (JniObjRef*) malloc(sizeof(JniObjRef));
 
-	ret = (*aEnv)->GetJavaVM(aEnv, &(*aRef)->vm);
-	if (ret < 0) {
-		printf("PropertyJNI: Unable to get reference to the current Java VM.\n");
-		fflush(stdout);
-	}
-	(*aRef)->callbackObj = (*aEnv)->NewGlobalRef(aEnv, aObject);
-	if ((*aRef)->callbackObj == NULL) {
-		printf("PropertyJNI: Callback object not stored.\n");
-		fflush(stdout);
-	}
+    ret = (*aEnv)->GetJavaVM(aEnv, &(*aRef)->vm);
+    if (ret < 0) {
+        printf("PropertyJNI: Unable to get reference to the current Java VM.\n");
+        fflush(stdout);
+    }
+    (*aRef)->callbackObj = (*aEnv)->NewGlobalRef(aEnv, aObject);
+    if ((*aRef)->callbackObj == NULL) {
+        printf("PropertyJNI: Callback object not stored.\n");
+        fflush(stdout);
+    }
 }
 
 /*
  * Class:     org_openhome_net_core_Property
- * Method:    ServicePropertyDestroy
- * Signature: (JJ)V
+ * Method:    ServicePropertyDestroyProperty
+ * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_org_openhome_net_core_Property_ServicePropertyDestroy
-  (JNIEnv *aEnv, jclass aClass, jlong aProperty, jlong aCallback)
+JNIEXPORT void JNICALL Java_org_openhome_net_core_Property_ServicePropertyDestroyProperty
+  (JNIEnv *aEnv, jclass aClass, jlong aProperty)
 {
-	ServiceProperty property = (ServiceProperty) (size_t)aProperty;
-	JniObjRef *ref = (JniObjRef*) (size_t)aCallback;
-	aClass = aClass;
-	
-	(*aEnv)->DeleteGlobalRef(aEnv, ref->callbackObj);
-	free(ref);
-	ServicePropertyDestroy(property);
+    ServiceProperty property = (ServiceProperty) (size_t)aProperty;
+    aEnv = aEnv;
+    aClass = aClass;
+    
+    ServicePropertyDestroy(property);
+}
+
+/*
+ * Class:     org_openhome_net_core_Property
+ * Method:    ServicePropertyDestroyCallback
+ * Signature: (J)V
+ */
+JNIEXPORT void JNICALL Java_org_openhome_net_core_Property_ServicePropertyDestroyCallback
+  (JNIEnv *aEnv, jclass aClass, jlong aCallback)
+{
+    JniObjRef *ref = (JniObjRef*) (size_t)aCallback;
+    aClass = aClass;
+    
+     (*aEnv)->DeleteGlobalRef(aEnv, ref->callbackObj);
+     free(ref);
 }
 
 #ifdef __cplusplus
--- OpenHome/Net/Bindings/Java/org/openhome/net/core/Property.java
+++ OpenHome/Net/Bindings/Java/org/openhome/net/core/Property.java
@@ -9,64 +9,75 @@ import org.openhome.net.controlpoint.IPropertyChangeListener;
  */
 public class Property
 {
-	private static native void ServicePropertyDestroy(long aProperty, long aCallback);
-	
-	static
+    private static native void ServicePropertyDestroyProperty(long aProperty);
+    private static native void ServicePropertyDestroyCallback(long aCallback);
+    
+    static
     {
         System.loadLibrary("ohNet");
         System.loadLibrary("ohNetJni");
     }
-	
-	protected class PropertyInitialised
-	{
-		private long iHandle;
-		private long iCallback;
-		
-		public PropertyInitialised(long aHandle, long aCallback)
-		{
-			iHandle = aHandle;
-			iCallback = aCallback;
-		}
-		public long getHandle()
-		{
-			return iHandle;
-		}
-		public long getCallback()
-		{
-			return iCallback;
-		}
-	}
-	
-	protected long iHandle;
-	protected long iCallback;
-	private boolean iOwnsNativeProperty;
-	private IPropertyChangeListener iValueChanged;
-	
-	protected Property()
-	{
-		iOwnsNativeProperty = false;
-	}
-	
-	protected Property(boolean aOwnsNativeProperty)
-	{
-		iOwnsNativeProperty = aOwnsNativeProperty;
-	}
-	
-	protected Property(IPropertyChangeListener aListener)
-	{
-		iValueChanged = aListener;
-	}
-	
-	public long getHandle()
-	{
-		return iHandle;
-	}
-	
-	public void destroy()
-	{
-		if (iOwnsNativeProperty)
-		{
-			ServicePropertyDestroy(iHandle, iCallback);
-		}
-	}
+    
+    protected class PropertyInitialised
+    {
+        private long iHandle;
+        private long iCallback;
+        
+        public PropertyInitialised(long aHandle, long aCallback)
+        {
+            iHandle = aHandle;
+            iCallback = aCallback;
+        }
+        public long getHandle()
+        {
+            return iHandle;
+        }
+        public long getCallback()
+        {
+            return iCallback;
+        }
+    }
+    
+    protected long iHandle;
+    protected long iCallback;
+    private boolean iOwnsNativeProperty;
+    private IPropertyChangeListener iValueChanged;
+    
+    protected Property()
+    {
+        iOwnsNativeProperty = false;
+    }
+    
+    protected Property(boolean aOwnsNativeProperty)
+    {
+        iOwnsNativeProperty = aOwnsNativeProperty;
+    }
+    
+    protected Property(IPropertyChangeListener aListener)
+    {
+        iValueChanged = aListener;
+    }
+    
+    public long getHandle()
+    {
+        return iHandle;
+    }
+    
+    public void destroy()
+    {
+        if (iOwnsNativeProperty)
+        {
+            // This is a device property object with a native property object (iHandle)
+            // and no callback object (iCallback).
+            ServicePropertyDestroyProperty(iHandle);
+        }
+        if (iCallback != 0)
+        {
+            // This is a control point property object with a callback object (iCallback).
+            // The native property object (iHandle) is freed by the CpProxy.disposeProxy()
+            // method, which is called by the dispose() method of the generated Java proxy
+            // before it calls this method.
+            ServicePropertyDestroyCallback(iCallback);
+        }
+    }
 }
