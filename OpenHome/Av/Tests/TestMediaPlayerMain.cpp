3637623ee8002e1482195e6092609a7e3f64c772
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2013-07-23 09:42:06 +0100

    Refactored TestMediaPlayer to take a function pointer for creation to break link time dependencies

--- OpenHome/Av/Tests/TestMediaPlayerMain.cpp
+++ OpenHome/Av/Tests/TestMediaPlayerMain.cpp
@@ -13,12 +13,12 @@ using namespace OpenHome;
 using namespace OpenHome::Av;
 using namespace OpenHome::Av::Test;
 
-TestMediaPlayer* OpenHome::Av::Test::CreateMediaPlayer(Net::DvStack& aDvStack, const Brx& aUdn, const TChar* aRoom, const TChar* aProductName, TUint aMaxDriverJiffies, const TChar* aTuneInUserName)
+TestMediaPlayer* CreateMediaPlayer(Net::DvStack& aDvStack, const Brx& aUdn, const TChar* aRoom, const TChar* aProductName, TUint aMaxDriverJiffies, const TChar* aTuneInUserName)
 {
     return new TestMediaPlayer(aDvStack, aUdn, aRoom, aProductName, aMaxDriverJiffies, aTuneInUserName);
 }
 
 int CDECL main(int aArgc, char* aArgv[])
 {
-    return OpenHome::Av::Test::ExecuteTestMediaPlayer(aArgc, aArgv);
+    return OpenHome::Av::Test::ExecuteTestMediaPlayer(aArgc, aArgv, CreateMediaPlayer);
 }
