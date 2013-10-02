5f78436f66efb1429dcf205882e5ddd9abe5d765
Simon Chisholm <simon.chisholm@linn.co.uk>
2013-10-02 11:09:39 +0100

    Allow ProtocolHttp to read as much data as is required by its worst case client

--- OpenHome/Media/Protocol/Protocol.h
+++ OpenHome/Media/Protocol/Protocol.h
@@ -85,7 +85,8 @@ private:
 class ProtocolNetwork : public Protocol
 {
 protected:
-    static const TUint kReadBufferBytes = 8 * 1024; // WMA radio streams (in ProtocolRtsp) require at least an 8k buffer
+    static const TUint kReadBufferBytes = 9 * 1024; // WMA radio streams (in ProtocolRtsp) require at least an 8k buffer
+                                                    // Songcast requires a 9k buffer
     static const TUint kWriteBufferBytes = 1024;
     static const TUint kConnectTimeoutMs = 3000;
 protected:
