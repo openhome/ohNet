3102d712866109cec90999edaa58a5073f900b28
Simon Chisholm <simon.chisholm@linn.co.uk>
2014-01-31 13:34:32 +0000

    Option to suppress error dialogs on Windows (copied from ohNet).  May be useful for integration tests.

--- OpenHome/Av/Tests/TestMediaPlayerMain.cpp
+++ OpenHome/Av/Tests/TestMediaPlayerMain.cpp
@@ -3,6 +3,8 @@
 #include <OpenHome/Media/Tests/Cdecl.h>
 #include <OpenHome/Media/Tests/GetCh.h>
 
+#include <stdlib.h>
+
 using namespace OpenHome;
 using namespace OpenHome::Av;
 using namespace OpenHome::Av::Test;
@@ -14,5 +16,11 @@ TestMediaPlayer* CreateMediaPlayer(Net::DvStack& aDvStack, const Brx& aUdn, cons
 
 int CDECL main(int aArgc, char* aArgv[])
 {
+#ifdef _WIN32
+    char* noErrDlgs = getenv("NO_ERROR_DIALOGS");
+    if (noErrDlgs != NULL && strcmp(noErrDlgs, "1") == 0) {
+        _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+    }
+#endif // _WIN32
     return OpenHome::Av::Test::ExecuteTestMediaPlayer(aArgc, aArgv, CreateMediaPlayer);
 }
