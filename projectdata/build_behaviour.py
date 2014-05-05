6402dbebf8696577b6fbac1ad9ae5b669e238189
grahamdo <graham.douglas@linn.co.uk>
2014-05-05 14:56:43 +0100

    Uprev devtools and FunctionalTest dependencies - devtools reqd to pick up larger FunctionalTest archive

--- projectdata/build_behaviour.py
+++ projectdata/build_behaviour.py
@@ -60,7 +60,7 @@ from ci import (
         build_condition, default_platform, get_dependency_args,
         get_vsvars_environment, fetch_dependencies, python, scp)
 
-require_version(22)
+require_version(35)
 
 
 # Command-line options. See documentation for Python's optparse module.
