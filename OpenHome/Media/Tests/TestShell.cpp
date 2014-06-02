5eeaf7d7f12a320370c4025e194abe89169f1cbd
Simon Chisholm <simon.chisholm@linn.co.uk>
2014-06-02 10:55:30 +0100

    Run Shell on a host-determined port.  Allows multiple instances of TestMediaPlayer to run on Linux.  Fixes #1961

--- OpenHome/Media/Tests/TestShell.cpp
+++ OpenHome/Media/Tests/TestShell.cpp
@@ -19,7 +19,7 @@ void OpenHome::Media::ExecuteTestShell(Net::InitialisationParams* aInitParams, s
     Endpoint endpt(0, addr);
     Endpoint::AddressBuf buf;
     endpt.AppendAddress(buf);
-    Log::Print("Connect to %s.%u then 'help' for options\n\n", buf.Ptr(), Shell::kServerPort);
+    Log::Print("Connect to %s.%u then 'help' for options\n\n", buf.Ptr(), Shell::kServerPortDefault);
     Library::DestroySubnetList(subnetList);
     CpStack* cpStack = NULL;
     DvStack* dvStack = NULL;
