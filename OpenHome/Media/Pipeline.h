1c7372f6d3989b8560481a00884f65cc8967be80
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2014-06-16 09:35:52 +0100

    Reduce Waiter ramping duration from 500ms to 50ms. Limits chance of running out of flush msgs if doing a large number of (read: excessive) seeks within a short period from an Airplay source

--- OpenHome/Media/Pipeline.h
+++ OpenHome/Media/Pipeline.h
@@ -56,7 +56,7 @@ class Pipeline : public ISupply, public IPipelineElementUpstream, public IFlushI
     static const TUint kSeekerRampDuration                   = Jiffies::kPerMs * 20;
     static const TUint kVariableDelayRampDuration            = Jiffies::kPerMs * 20;
     static const TUint kSkipperRampDuration                  = Jiffies::kPerMs * 500;
-    static const TUint kWaiterRampDuration                   = Jiffies::kPerMs * 500;
+    static const TUint kWaiterRampDuration                   = Jiffies::kPerMs * 50;
     static const TUint kStopperRampDuration                  = Jiffies::kPerMs * 500;
     static const TUint kGorgerDuration                       = Jiffies::kPerMs * 1000;
     static const TUint kStarvationMonitorNormalSize          = Jiffies::kPerMs * 50;
