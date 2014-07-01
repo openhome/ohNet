76003432053c4767e03b39c79b68e21a7d441f7e
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2014-07-01 09:57:32 +0100

    Removed "using namespace OpenHome" directive from IStore.h

--- OpenHome/Configuration/IStore.h
+++ OpenHome/Configuration/IStore.h
@@ -2,16 +2,16 @@
 #define HEADER_ISTORE
 
 #include <OpenHome/Exception.h>
-#include <OpenHome/Buffer.h>
 
 EXCEPTION(StoreKeyNotFound);
 EXCEPTION(StoreReadBufferUndersized);
 EXCEPTION(StoreWriteAllocationFailed);
 
 namespace OpenHome {
-namespace Configuration {
+    class Brx;
+    class Bwx;
 
-using namespace OpenHome;
+namespace Configuration {
 
 /*
 The zero-length key is considered invalid. API usage with a zero-length key
