fb39cfd2bfdea845a62278cdc4d128e72929858f
grahamdo <graham.douglas@linn.co.uk>
2014-07-02 14:02:32 +0100

    Preparation for nightly integration-test-full runs Add build-step and some required parameters

--- IntegrationTests/SuiteLocal.py
+++ IntegrationTests/SuiteLocal.py
@@ -3,13 +3,17 @@
 
 Parameters:
     arg#1 - UPnP Media Server to use for tests
-    arg#2 - [Optional] log directory
+    arg#1 - DACP (iTunes) server PC
+    arg#3 - [Optional] log directory
 
-This suite of tests verifies ohMediaPlayer functionality using SoftPlayer(s)
+This suite of tests verifies ohMediaPlayer functionality using SoftPlayer(s).
+Loopback adapter is used where practical (so all cases except for Airplay and
+where external media server is required - ohNet does not allow mix of local
+loopback and standard adapter devices within single process space)
 
  - Network interface to use for testing defined in local Config.xml
  - SoftPlayer instance(s) created/destryed as required
- - iTunes must be running on local machine (for Airplay testing)
+ - iTunes server must be available (for Airplay testing)
  - UPnP media server with reqd. playlists must be available (for playlist soaks) 
 """
 import _FunctionalTest
@@ -17,17 +21,18 @@ import BaseSuite as Suite
 import os
 import sys
  
-ms         = None
-logDir     = None
-dacpServer = 'PC718'
-avtList    = 'Playlists  ~Encodings-FreeCodecs'
-mpList     = 'Playlists  ~Mixed-FreeCodecs'
-apTrack    = 'Stereo1kHz-441-16-0dB-72m' 
+ms      = None
+logDir  = None
+dacp    = None
+avtList = 'Playlists  ~Encodings-FreeCodecs'
+mpList  = 'Playlists  ~Mixed-FreeCodecs'
+apTrack = 'Stereo1kHz-441-16-0dB-72m'
 
 try:
     ms = sys.argv[1]
-    if len( sys.argv ) > 2:
-        logDir = sys.argv[2]
+    dacp = sys.argv[2]
+    if len( sys.argv ) > 3:
+        logDir = sys.argv[3]
 except:
     print '\n', __doc__, '\n'
     print '\n\nInvalid arguments %s' % (str( sys.argv ))
@@ -37,40 +42,40 @@ except:
     
 tests = [
     # UPnP AV          
-    # [ 'TestAvTransportService',      'local', 'ALL'                                                             ],
-    # [ 'TestRenderingControlService', 'local'                                                                    ],
-    [ 'TestAvTransportPlayTracks',   'local', 'local',    ms,       avtList,  8,    1,         'false'          ],
+    [ 'TestAvTransportService',      'local', 'ALL'                                                          ],
+    [ 'TestRenderingControlService', 'local'                                                                 ],
+    [ 'TestAvTransportPlayTracks',   'local', 'local', ms,       avtList,  8,    1,         'false'          ],
 
     # OH Playlist Service
-    [ 'TestPlaylistHandling',        'local', 'all',      12345                                                 ],
-    [ 'TestPlaylistModes',           'local', 'all',      54321                                                 ],
-    [ 'TestPlaylistPlayback',        'local', 'all',      12345                                                 ],
-    [ 'TestPlaylistAddDelSoak',      'local', ms,         mpList,   100                                         ],
-    [ 'TestPlaylistPlayTracks',      'local', 'local',    ms,       mpList,   8,    'off',     'on',    'false' ],
-    [ 'TestPlaylistDropout',         'local', 'local',    'local',  '3600'                                      ],
+    [ 'TestPlaylistHandling',        'local', 'all',   12345                                                 ],
+    [ 'TestPlaylistModes',           'local', 'all',   54321                                                 ],
+    [ 'TestPlaylistPlayback',        'local', 'all',   12345                                                 ],
+    [ 'TestPlaylistAddDelSoak',      'local', ms,      mpList,   100                                         ],
+    [ 'TestPlaylistPlayTracks',      'local', 'local', ms,       mpList,   8,    'off',     'on',    'false' ],
+    [ 'TestPlaylistDropout',         'local', 'local', 'local',  '3600'                                      ],
 
     # OH Radio Service
-    [ 'TestRadioService',            'local', 'all'                                                             ],
-    [ 'TestRadioPlayChannels',       'local', 'local',    'ohmp2',  20,       'on', 1                           ],
+    [ 'TestRadioService',            'local', 'all'                                                          ],
+    [ 'TestRadioPlayChannels',       'local', 'local', 'ohmp2',  20,       'on', 1                           ],
     
-    # # Airplay
-    # [ 'TestAirplayFunctions',        'local', dacpServer                                                        ],
-    # [ 'TestAirplayDropout',          'local', dacpServer, apTrack,  '3600'                                      ],
+    # Airplay
+    [ 'TestAirplayFunctions',        'local', dacp                                                           ],
+    [ 'TestAirplayDropout',          'local', dacp,    apTrack,  '3600'                                      ],
 
     # # Songcast
-    # [ 'TestSongcastPlayback',        'local', 'local',    'local',  'all',   12345                              ],
-    # [ 'TestSongcastConnect',         'local', 'local',    'local',  'local', 'all', 'stop'                      ],
-    # [ 'TestSongcastConnect',         'local', 'local',    'local',  'local', 'all', 'source'                    ],
-    # [ 'TestSongcastConnect',         'local', 'local',    'local',  'local', 'all', 'sender'                    ],
-    # [ 'TestSongcastConnect',         'local', 'local',    'local',  'local', 'all', 'standby'                   ],
-    # [ 'TestSongcastDropout',         'local', 'local',    600,                                                  ],
+    # [ 'TestSongcastPlayback',        'local', 'local', 'local',  'all',   12345                              ],
+    # [ 'TestSongcastConnect',         'local', 'local', 'local',  'local', 'all', 'stop'                      ],
+    # [ 'TestSongcastConnect',         'local', 'local', 'local',  'local', 'all', 'source'                    ],
+    # [ 'TestSongcastConnect',         'local', 'local', 'local',  'local', 'all', 'sender'                    ],
+    # [ 'TestSongcastConnect',         'local', 'local', 'local',  'local', 'all', 'standby'                   ],
+    # [ 'TestSongcastDropout',         'local', 'local', 600,                                                  ],
 
     # Volume
-    [ 'TestVolumeControl',           'local'                                                                    ],
+    [ 'TestVolumeControl',           'local'                                                                 ],
     
     # # Misc
-    # [ 'TestAudioConx',               'local', 'all'                                                             ],
-    # [ 'TestStandbyCycleSoak',        'local',  100                                                              ]
+    # [ 'TestAudioConx',               'local', 'all'                                                          ],
+    # [ 'TestStandbyCycleSoak',        'local',  100                                                           ]
     
     # N/A to SoftPlayer(s)
     #    - AutoPlay
@@ -84,4 +89,4 @@ tests = [
     #    - VolumeXXX   (apart from volume/balance)
 ]
 
-Suite.Suite( tests, logDir, ['config','status','info'] )
+Suite.Suite( tests, logDir, ['config','status'] )
