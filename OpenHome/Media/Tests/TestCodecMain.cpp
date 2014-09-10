141643132c4d84e36e0de913eeaeaf4ae60cbc51
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2014-09-10 11:46:20 +0100

    Override '-s' parameter in TestCodecMain to use localhost instead. Doesn't affect Core platforms, as they are not instantiated via TestCodecMain.

--- OpenHome/Media/Tests/TestCodecMain.cpp
+++ OpenHome/Media/Tests/TestCodecMain.cpp
@@ -12,8 +12,26 @@ extern AudioFileCollection* TestCodecFiles();
 
 void OpenHome::TestFramework::Runner::Main(TInt aArgc, TChar* aArgv[], Net::InitialisationParams* aInitParams)
 {
+    static const Brn kLocalhost("127.0.0.1");
     Net::Library* lib = new Net::Library(aInitParams);
     std::vector<Brn> args = OptionParser::ConvertArgs(aArgc, aArgv);
+
+    // This is an override to force all instantiations to run via localhost
+    // regardless of '-s' arg.
+    // (Core platforms are not instantiated via this Main() method, so they
+    // remain unaffected.)
+    for (TUint i=0; i<args.size(); i++) {
+        if (args[i] == Brn("-s")) {
+            TUint hostIdx = i+1;
+            ASSERT(hostIdx < args.size());
+            Brn hostOld = args[hostIdx];
+            args[hostIdx] = kLocalhost;
+            Log::Print("OVERRIDE: TestCodec using '127.0.0.1' instead of argument '");
+            Log::Print(hostOld);
+            Log::Print("'\n");
+            break;
+        }
+    }
     TestCodec(lib->Env(), CreateTestCodecPipeline, TestCodecFiles, args);
     delete lib;
 }
