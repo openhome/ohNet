714df22c349ead6c62b18b2a9fb93655a43913ca
Stathis Voukelatos <stathis.voukelatos@linn.co.uk>
2014-10-24 10:58:05 +0100

    Added support for Qnap-armel builds

--- bundle_binaries.py
+++ bundle_binaries.py
@@ -50,8 +50,10 @@ ALL_TARGETS = {
         BuildTarget("Linux",   "armhf", "Release"): BuildInfo(builddir="Build/Obj/Posix/Release"),
         BuildTarget("Linux",   "ppc32", "Release"): BuildInfo(builddir="Build/Obj/Posix/Release"),
         
-        BuildTarget("Qnap",   "x86",   "Debug"):    BuildInfo(builddir="Build/Obj/Posix/Debug"),
-        BuildTarget("Qnap",   "x86",   "Release"):  BuildInfo(builddir="Build/Obj/Posix/Release"),
+        BuildTarget("Qnap",   "x86",    "Debug"):   BuildInfo(builddir="Build/Obj/Posix/Debug"),
+        BuildTarget("Qnap",   "x86",    "Release"): BuildInfo(builddir="Build/Obj/Posix/Release"),
+        BuildTarget("Qnap",   "armel",  "Debug"):   BuildInfo(builddir="Build/Obj/Posix/Debug"),
+        BuildTarget("Qnap",   "armel",  "Release"): BuildInfo(builddir="Build/Obj/Posix/Release"),
 
         BuildTarget("Mac",     "x86",   "Debug"):   BuildInfo(builddir="Build/Obj/Mac-x86/Debug"),
         BuildTarget("Mac",     "x64",   "Debug"):   BuildInfo(builddir="Build/Obj/Mac-x64/Debug"),
