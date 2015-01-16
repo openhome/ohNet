bb96f2f3070af66453d9b2f7ba701ce9312c0466
Simon Chisholm <simon.chisholm@linn.co.uk>
2015-01-16 12:02:22 +0000

    Report different exceptions for SEH errors that seem most relevant

--- Os/Windows/SignalHandlers.cpp
+++ Os/Windows/SignalHandlers.cpp
@@ -2,12 +2,38 @@
 #include <OpenHome/Exception.h>
 
 #include <windows.h>
+#include <stdio.h>
 
 EXCEPTION(Windows_SEH)
+EXCEPTION(Windows_SEH_AccessViolation)
+EXCEPTION(Windows_SEH_ArrayBoundsExceeded)
+EXCEPTION(Windows_SEH_DatatypeMisalignment)
+EXCEPTION(Windows_SEH_GuardPage)
+EXCEPTION(Windows_SEH_DivideByZero)
+EXCEPTION(Windows_SEH_InvalidHandle)
+EXCEPTION(Windows_SEH_StackOverflow)
 
-static void SEHTranslator(unsigned int /*u*/, EXCEPTION_POINTERS* /*pExp*/)
+static void SEHTranslator(unsigned int u, EXCEPTION_POINTERS* /*pExp*/)
 {
-    THROW(OpenHome::Windows_SEH);
+    switch (u)
+    {
+    case EXCEPTION_ACCESS_VIOLATION:
+        THROW(OpenHome::Windows_SEH_AccessViolation);
+    case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
+        THROW(OpenHome::Windows_SEH_ArrayBoundsExceeded);
+    case EXCEPTION_DATATYPE_MISALIGNMENT:
+        THROW(OpenHome::Windows_SEH_DatatypeMisalignment);
+    case EXCEPTION_GUARD_PAGE:
+        THROW(OpenHome::Windows_SEH_GuardPage);
+    case EXCEPTION_INT_DIVIDE_BY_ZERO:
+        THROW(OpenHome::Windows_SEH_DivideByZero);
+    case EXCEPTION_INVALID_HANDLE:
+        THROW(OpenHome::Windows_SEH_InvalidHandle);
+    case EXCEPTION_STACK_OVERFLOW:
+        THROW(OpenHome::Windows_SEH_StackOverflow);
+    default:
+        THROW(OpenHome::Windows_SEH);
+    }
 }
 
 extern "C" void OsThreadInstallSignalHandlers()
