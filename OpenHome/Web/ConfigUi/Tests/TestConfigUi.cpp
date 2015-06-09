97b02a12ece81e55a639592bf6ad371b95998a31
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2015-06-09 15:48:25 +0100

    Introduce UriRetriever and WriterPrinter classes into TestConfigUi.

--- OpenHome/Web/ConfigUi/Tests/TestConfigUi.cpp
+++ OpenHome/Web/ConfigUi/Tests/TestConfigUi.cpp
@@ -5,6 +5,7 @@
 #include <OpenHome/Net/Private/CpiStack.h>
 #include <OpenHome/Net/Private/DviStack.h>
 #include <OpenHome/Private/Parser.h>
+#include <OpenHome/Private/Uri.h>
 #include <OpenHome/Net/Private/XmlParser.h>
 #include <OpenHome/Net/Private/Ssdp.h>
 #include <OpenHome/Media/Utils/DriverBasic.h>
@@ -31,6 +32,49 @@ private:
     mutable OpenHome::Mutex iLock;
 };
 
+class UriRetriever
+{
+private:
+    static const TUint kReadBufferBytes = 1024;
+    static const TUint kWriteBufferBytes = 1024;
+    static const TUint kMaxResponseChunkBytes = 1024;
+    static const TUint kConnectTimeoutMs = 3000;
+public:
+    UriRetriever(Environment& aEnv, const Uri& aBaseUri);
+    TUint Retrieve(const Brx& aTail, const Brx& aMethod, const Brx& aRequest, IWriter& aResponseWriter);
+private:
+    TUint RetrieveUriSocketOpen(const Brx& aMethod, const Brx& aRequest, IWriter& aResponseWriter);
+    void SetUriBase(const Uri& aUri);
+private:
+    Environment& iEnv;
+    Bws<Uri::kMaxUriBytes> iUriBaseBuf;
+    Uri iUri;
+    SocketTcpClient iTcpClient;
+    Srs<kReadBufferBytes> iReaderBuf;
+    ReaderUntilS<kReadBufferBytes> iReaderUntil;
+    Sws<kWriteBufferBytes> iWriterBuf;
+    TBool iSocketIsOpen;
+
+    WriterHttpRequest iWriterRequest;
+    ReaderHttpResponse iReaderResponse;
+    //ReaderHttpChunked iDechunker;
+    HttpHeaderContentType iHeaderContentType;
+    HttpHeaderContentLength iHeaderContentLength;
+};
+
+class WriterPrinter : public IWriter
+{
+public:
+    WriterPrinter();
+    TUint BytesPrinted() const;
+    void Reset();
+public: // from IWriter
+    void Write(TByte aValue) override;
+    void Write(const Brx& aBuffer) override;
+    void WriteFlush() override;
+private:
+    TUint iBytesPrinted;
+};
 
 // ConfigApps are not expected to check the existence of ConfigVals at startup.
 // This means that the ConfigManager will assert when the config page is loaded
@@ -50,9 +94,6 @@ class SuiteConfigUi : public OpenHome::TestFramework::SuiteUnitTest, public Open
 private:
     static const TUint kMaxUiTabs = 4;
     static const TUint kUiSendQueueSize = 32;
-    static const TUint kReadBufferBytes = 1024;
-    static const TUint kWriteBufferBytes = 1024;
-    static const TUint kConnectTimeoutMs = 3000;
 public:
     SuiteConfigUi(OpenHome::Net::CpStack& aCpStack, OpenHome::Net::DvStack& aDvStack);
 private: // from SuiteUnitTest
@@ -60,12 +101,6 @@ private: // from SuiteUnitTest
     void TearDown();
 private:
     void Run();
-    void OpenSocket();
-    void CloseSocket();
-    TUint RetrieveUri(const OpenHome::Uri& aUri, const OpenHome::Brx& aMethod, const OpenHome::Brx& aDataRequest, OpenHome::Bwx& aDataResponse);
-    TUint RetrieveUriSocketOpen(const OpenHome::Uri& aUri, const OpenHome::Brx& aMethod, const OpenHome::Brx& aDataRequest, OpenHome::Bwx& aDataResponse);
-    void GetUriBase(const OpenHome::Uri& aUri, OpenHome::Bwx& aBufOut);
-    void ConstructUri(const OpenHome::Brx& aBase, const OpenHome::Brx& aTail, OpenHome::Uri& aUriOut);
     void TestGetStaticResource();
     void TestLongPollCreate();
     void TestLongPoll();
@@ -79,19 +114,6 @@ private:
 
     WebAppFramework* iAppFramework;
     ConfigAppMediaPlayer* iConfigApp;
-
-    OpenHome::Srs<kReadBufferBytes>* iReaderBuf;
-    OpenHome::ReaderUntil* iReaderUntil;
-    OpenHome::Sws<kWriteBufferBytes>* iWriterBuf;
-    OpenHome::Mutex* iLock;
-    OpenHome::SocketTcpClient* iTcpClient;
-    TBool iSocketIsOpen;
-
-    OpenHome::WriterHttpRequest* iWriterRequest;
-    OpenHome::ReaderHttpResponse* iReaderResponse;
-    //OpenHome::ReaderHttpChunked* iDechunker;
-    OpenHome::HttpHeaderContentType* iHeaderContentType;
-    OpenHome::HttpHeaderContentLength* iHeaderContentLength;
 };
 
 } // namespace Test
@@ -146,6 +168,148 @@ const Brx& HelperDeviceListHandler::GetPresentationUrl() const
 }
 
 
+// UriRetriever
+
+UriRetriever::UriRetriever(Environment& aEnv, const Uri& aBaseUri)
+    : iEnv(aEnv)
+    , iReaderBuf(iTcpClient)
+    , iReaderUntil(iReaderBuf)
+    , iWriterBuf(iTcpClient)
+    , iSocketIsOpen(false)
+    , iWriterRequest(iWriterBuf)
+    , iReaderResponse(iEnv, iReaderUntil)
+{
+    iReaderResponse.AddHeader(iHeaderContentType);
+    iReaderResponse.AddHeader(iHeaderContentLength);
+    SetUriBase(aBaseUri);
+}
+
+TUint UriRetriever::Retrieve(const Brx& aTail, const Brx& aMethod, const Brx& aRequest, IWriter& aResponseWriter)
+{
+    iUri.Replace(iUriBaseBuf, aTail);
+
+    Log::Print("UriRetriever::Retrieve: ");
+    Log::Print(iUri.AbsoluteUri());
+    Log::Print("\n");
+
+    iTcpClient.Open(iEnv);
+    TUint code = RetrieveUriSocketOpen(aMethod, aRequest, aResponseWriter);
+    iTcpClient.Close();
+    return code;
+}
+
+TUint UriRetriever::RetrieveUriSocketOpen(const Brx& aMethod, const Brx& aRequest, IWriter& aResponseWriter)
+{
+    TUint code = 0;
+    Endpoint ep(iUri.Port(), iUri.Host());
+
+    try {
+        iTcpClient.Connect(ep, kConnectTimeoutMs);
+    }
+    catch (NetworkTimeout&) {
+        return code;
+    }
+    catch (NetworkError&) {
+        return code;
+    }
+
+    try {
+        iWriterRequest.WriteMethod(aMethod, iUri.PathAndQuery(), Http::eHttp11);
+        const TUint port = (iUri.Port() == -1 ? 80 : (TUint)iUri.Port());
+        Http::WriteHeaderHostAndPort(iWriterRequest, iUri.Host(), port);
+        Http::WriteHeaderContentLength(iWriterRequest, aRequest.Bytes());
+        Http::WriteHeaderConnectionClose(iWriterRequest);
+        iWriterRequest.WriteFlush();
+        iWriterBuf.Write(aRequest);
+        iWriterBuf.WriteFlush();
+    }
+    catch(WriterError&) {
+        return code;
+    }
+
+    try {
+        iReaderResponse.Read();
+
+        code = iReaderResponse.Status().Code();
+        if (code == HttpStatus::kOk.Code()) {
+            // Content-Length currently returns 0 so read until ReaderError
+            try {
+                for (;;) {
+                    Brn buf = iReaderUntil.Read(kMaxResponseChunkBytes);
+                    if (buf.Bytes() == 0) {
+                        // Reached end of data.
+                        return code;
+                    }
+                    aResponseWriter.Write(buf);
+                }
+            }
+            catch (ReaderError&) {
+            }
+            catch (WriterError&) {
+            }
+        }
+    }
+    catch(HttpError&) {
+        return code;
+    }
+    catch(ReaderError&) {
+        return code;
+    }
+
+    return code;
+}
+
+void UriRetriever::SetUriBase(const Uri& aUri)
+{
+    iUriBaseBuf.Replace(aUri.Scheme());
+    iUriBaseBuf.Append("://");
+    iUriBaseBuf.Append(aUri.Host());
+    iUriBaseBuf.Append(":");
+    Ascii::AppendDec(iUriBaseBuf, aUri.Port());
+    iUriBaseBuf.Append("/");
+
+    Parser p(aUri.Path());
+    p.Next('/');                        // skip '/' at start
+    iUriBaseBuf.Append(p.Next('/'));    // append resource prefix
+    iUriBaseBuf.Append("/");
+}
+
+
+// WriterPrinter
+
+WriterPrinter::WriterPrinter()
+    : iBytesPrinted(0)
+{
+}
+
+TUint WriterPrinter::BytesPrinted() const
+{
+    return iBytesPrinted;
+}
+
+void WriterPrinter::Reset()
+{
+    iBytesPrinted = 0;
+}
+
+void WriterPrinter::Write(TByte aValue)
+{
+    Log::Print("%c", aValue);
+    iBytesPrinted += 1;
+}
+
+void WriterPrinter::Write(const Brx& aBuffer)
+{
+    Log::Print(aBuffer);
+    iBytesPrinted += aBuffer.Bytes();
+}
+
+void WriterPrinter::WriteFlush()
+{
+    Log::Flush();
+}
+
+
 // SuiteConfigUi
 
 // FIXME - take resource dir as param
@@ -197,23 +361,6 @@ void SuiteConfigUi::Setup()
         delete blocker;
         delete deviceList;
     }
-
-    // TCP client setup.
-    iLock = new Mutex ("SCOM");
-    iTcpClient = new SocketTcpClient();
-    iReaderBuf = new Srs<kReadBufferBytes>(*iTcpClient);
-    iReaderUntil = new ReaderUntilS<kReadBufferBytes>(*iReaderBuf);
-    iWriterBuf = new Sws<kWriteBufferBytes>(*iTcpClient);
-    iSocketIsOpen = false;
-
-    iHeaderContentType = new HttpHeaderContentType();
-    iHeaderContentLength = new HttpHeaderContentLength();
-
-    iWriterRequest = new WriterHttpRequest(*iWriterBuf);
-    iReaderResponse = new ReaderHttpResponse(iDvStack.Env(), *iReaderUntil);
-
-    iReaderResponse->AddHeader(*iHeaderContentType);
-    iReaderResponse->AddHeader(*iHeaderContentLength);
 }
 
 void SuiteConfigUi::TearDown()
@@ -223,17 +370,6 @@ void SuiteConfigUi::TearDown()
     // CredentialsThread to be run after Credentials destructor has already
     // been called.
     Thread::Sleep(1000);
-
-    delete iReaderResponse;
-    delete iWriterRequest;
-    delete iHeaderContentLength;
-    delete iHeaderContentType;
-
-    delete iLock;
-    delete iWriterBuf;
-    delete iReaderUntil;
-    delete iReaderBuf;
-    delete iTcpClient;
 
     iMediaPlayer->StopPipeline();
     delete iDeviceListHandler;
@@ -247,97 +383,6 @@ void SuiteConfigUi::Run()
     iMediaPlayer->RunWithSemaphore();
 }
 
-TUint SuiteConfigUi::RetrieveUri(const Uri& aUri, const Brx& aMethod, const Brx& aDataRequest, Bwx& aDataResponse)
-{
-    iTcpClient->Open(iDvStack.Env());
-    TUint code = RetrieveUriSocketOpen(aUri, aMethod, aDataRequest, aDataResponse);
-    iTcpClient->Close();
-    return code;
-}
-
-TUint SuiteConfigUi::RetrieveUriSocketOpen(const Uri& aUri, const Brx& aMethod, const Brx& aDataRequest, Bwx& aDataResponse)
-{
-    TUint code = 0;
-    Endpoint ep(aUri.Port(), aUri.Host());
-
-    try {
-        iTcpClient->Connect(ep, kConnectTimeoutMs);
-    }
-    catch (NetworkTimeout&) {
-        return code;
-    }
-    catch (NetworkError&) {
-        return code;
-    }
-
-    try {
-        iWriterRequest->WriteMethod(aMethod, aUri.PathAndQuery(), Http::eHttp11);
-        const TUint port = (aUri.Port() == -1 ? 80 : (TUint)aUri.Port());
-        Http::WriteHeaderHostAndPort(*iWriterRequest, aUri.Host(), port);
-        Http::WriteHeaderContentLength(*iWriterRequest, aDataRequest.Bytes());
-        Http::WriteHeaderConnectionClose(*iWriterRequest);
-        iWriterRequest->WriteFlush();
-        iWriterBuf->Write(aDataRequest);
-        iWriterBuf->WriteFlush();
-    }
-    catch(WriterError&) {
-        return code;
-    }
-
-    try {
-        iReaderResponse->Read();
-
-        code = iReaderResponse->Status().Code();
-        if (code == HttpStatus::kOk.Code()) {
-            // Content-Length currently returns 0 so read until ReaderError
-            aDataResponse.SetBytes(0);
-            try {
-                for (;;) {
-                    Brn buf = iReaderUntil->Read(aDataResponse.MaxBytes() - aDataResponse.Bytes());
-                    if (buf.Bytes() == 0) {
-                        // Reached end of data.
-                        return code;
-                    }
-                    aDataResponse.Append(buf);
-                }
-            }
-            catch (ReaderError&) {
-            }
-        }
-    }
-    catch(HttpError&) {
-        return code;
-    }
-    catch(ReaderError&) {
-        return code;
-    }
-
-    return code;
-}
-
-void SuiteConfigUi::GetUriBase(const Uri& aUri, Bwx& aBufOut)
-{
-    aBufOut.Replace(aUri.Scheme());
-    aBufOut.Append("://");
-    aBufOut.Append(aUri.Host());
-    aBufOut.Append(":");
-    Ascii::AppendDec(aBufOut, aUri.Port());
-    aBufOut.Append("/");
-
-    Parser p(aUri.Path());
-    p.Next('/');                    // skip '/' at start
-    aBufOut.Append(p.Next('/'));    // append resource prefix
-    aBufOut.Append("/");
-}
-
-void SuiteConfigUi::ConstructUri(const Brx& aBase, const Brx& aTail, Uri& aUriOut)
-{
-    Bws<Uri::kMaxUriBytes> uri;
-    uri.Append(aBase);
-    uri.Append(aTail);
-    aUriOut.Replace(uri);
-}
-
 void SuiteConfigUi::TestGetStaticResource()
 {
     const Brx& url = iDeviceListHandler->GetPresentationUrl();
@@ -347,8 +392,10 @@ void SuiteConfigUi::TestGetStaticResource()
     Log::Print("\n");
 
     const Uri uri(url);
-    Bws<kReadBufferBytes> data;
-    TUint code = RetrieveUri(uri, Http::kMethodGet, Brx::Empty(), data);
+    UriRetriever uriRetriever(iDvStack.Env(), uri);
+    Bws<1024> responseBuffer;
+    WriterBuffer writerBuf(responseBuffer);
+    TUint code = uriRetriever.Retrieve(Brn("index.html"), Http::kMethodGet, Brx::Empty(), writerBuf);
     TEST (code == HttpStatus::kOk.Code());
     // Check document looks like:
     // <!DOCTYPE ...>
@@ -356,7 +403,7 @@ void SuiteConfigUi::TestGetStaticResource()
     // ...
     // </html>
 
-    Parser p(data);
+    Parser p(responseBuffer);
     p.Next('!');    // skip "<!"
     Brn docType = p.Next();
     TEST(docType == Brn("DOCTYPE"));
@@ -379,90 +426,49 @@ void SuiteConfigUi::TestGetStaticResource()
 
 void SuiteConfigUi::TestLongPollCreate()
 {
+    static const Brn kExpectedSessionId("session-id: 1");
     const Brx& url = iDeviceListHandler->GetPresentationUrl();
     TEST(url.Bytes() > 0);
     const Uri uri(url);
 
-    Bws<Uri::kMaxUriBytes> uriBase;
-    GetUriBase(uri, uriBase);
-    Uri uriLpCreate;
-    ConstructUri(uriBase, Brn("lpcreate"), uriLpCreate);
-    Log::Print("SuiteConfigUi::TestLongPollCreate uriLpCreate: ");
-    Log::Print(uriLpCreate.AbsoluteUri());
-    Log::Print("\n");
-
-    Bws<kReadBufferBytes> data;
-    TUint code = RetrieveUri(uriLpCreate, Http::kMethodPost, Brx::Empty(), data);
-    TEST (code == HttpStatus::kOk.Code());
-
-    Log::Print("data: ");
-    Log::Print(data);
-    Log::Print("\n");
-
-    static const Brn kExpectedSessionId("session-id: 1");
-    TEST(data == kExpectedSessionId);
+    UriRetriever uriRetriever(iDvStack.Env(), uri);
+    Bws<1024> responseBuffer;
+    WriterBuffer writerBuf(responseBuffer);
+    TUint code = uriRetriever.Retrieve(Brn("lpcreate"), Http::kMethodPost, Brx::Empty(), writerBuf);
+    TEST(code == HttpStatus::kOk.Code());
+    TEST(responseBuffer == kExpectedSessionId);
 
 
     // FIXME - if test ends without requesting /lpterminate, TestMediaPlayer crashes during
     // destruction (looks like it's related to deleting ConfigVals before all
     // observers have unsubscribed).
-
-    Uri uriLpTerminate;
-    ConstructUri(uriBase, Brn("lpterminate"), uriLpTerminate);
-    Log::Print("SuiteConfigUi::TestLongPollCreate uriLpTerminate: ");
-    Log::Print(uriLpTerminate.AbsoluteUri());
-    Log::Print("\n");
-
-    code = RetrieveUri(uriLpTerminate, Http::kMethodPost, kExpectedSessionId, data);
-    TEST (code == HttpStatus::kOk.Code());
+    WriterPrinter writerPrinter;
+    code = uriRetriever.Retrieve(Brn("lpterminate"), Http::kMethodPost, kExpectedSessionId, writerPrinter);
+    TEST(code == HttpStatus::kOk.Code());
 }
 
 void SuiteConfigUi::TestLongPoll()
 {
+    static const Brn kExpectedSessionId("session-id: 1");
     const Brx& url = iDeviceListHandler->GetPresentationUrl();
     TEST(url.Bytes() > 0);
     const Uri uri(url);
 
-    Bws<Uri::kMaxUriBytes> uriBase;
-    GetUriBase(uri, uriBase);
-    Uri uriLpCreate;
-    ConstructUri(uriBase, Brn("lpcreate"), uriLpCreate);
-    Log::Print("SuiteConfigUi::TestLongPoll uriLpCreate: ");
-    Log::Print(uriLpCreate.AbsoluteUri());
-    Log::Print("\n");
-
-    Bws<kReadBufferBytes> data;
-    TUint code = RetrieveUri(uriLpCreate, Http::kMethodPost, Brx::Empty(), data);
-    TEST (code == HttpStatus::kOk.Code());
-
-    Log::Print("data: ");
-    Log::Print(data);
-    Log::Print("\n");
-
-    static const Brn kExpectedSessionId("session-id: 1");
-    TEST(data == kExpectedSessionId);
-
-    Uri uriLp;
-    ConstructUri(uriBase, Brn("lp"), uriLp);
-    Log::Print("SuiteConfigUi::TestLongPoll uriLp: ");
-    Log::Print(uriLp.AbsoluteUri());
-    Log::Print("\n");
-    code = RetrieveUri(uriLp, Http::kMethodPost, kExpectedSessionId, data);
+    UriRetriever uriRetriever(iDvStack.Env(), uri);
+    Bws<1024> responseBuffer;
+    WriterBuffer writerBuf(responseBuffer);
+    TUint code = uriRetriever.Retrieve(Brn("lpcreate"), Http::kMethodPost, Brx::Empty(), writerBuf);
     TEST(code == HttpStatus::kOk.Code());
-    TEST(data.Bytes() > 0);
-    Log::Print("SuiteConfigUi::TestLongPoll lp partial data response. Displaying %u bytes:\n", data.Bytes());
-    Log::Print(data);
-    Log::Print("\n");
+    TEST(responseBuffer == kExpectedSessionId);
 
+    WriterPrinter writerPrinter;
+    code = uriRetriever.Retrieve(Brn("lp"), Http::kMethodPost, kExpectedSessionId, writerPrinter);
+    TEST(code == HttpStatus::kOk.Code());
+    TEST(writerPrinter.BytesPrinted() > 0);
+    writerPrinter.Reset();
 
-    Uri uriLpTerminate;
-    ConstructUri(uriBase, Brn("lpterminate"), uriLpTerminate);
-    Log::Print("SuiteConfigUi::TestLongPoll uriLpTerminate: ");
-    Log::Print(uriLpTerminate.AbsoluteUri());
-    Log::Print("\n");
-
-    code = RetrieveUri(uriLpTerminate, Http::kMethodPost, kExpectedSessionId, data);
-    TEST (code == HttpStatus::kOk.Code());
+    code = uriRetriever.Retrieve(Brn("lpterminate"), Http::kMethodPost, kExpectedSessionId, writerPrinter);
+    TEST(code == HttpStatus::kOk.Code());
 }
 
 
