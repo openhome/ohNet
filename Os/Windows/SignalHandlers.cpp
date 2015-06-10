3ecb7568e52524cc9086c01b0462ffecc19e2adb
Simon Chisholm <simon.chisholm@linn.co.uk>
2015-06-10 13:46:28 +0100

    Translate all SEH exceptions into AssertionFailed.  This reduces the number of special cases for any clients who catch Exception& or ... - we can still determine the type of SEH error by checking the line number the exception was generated from.

--- Os/Windows/SignalHandlers.cpp
+++ Os/Windows/SignalHandlers.cpp
@@ -1,38 +1,31 @@
 #include <OpenHome/Os.h>
 #include <OpenHome/Exception.h>
+#include <OpenHome/Private/Standard.h>
 
 #include <windows.h>
 #include <stdio.h>
 
-EXCEPTION(Windows_SEH)
-EXCEPTION(Windows_SEH_AccessViolation)
-EXCEPTION(Windows_SEH_ArrayBoundsExceeded)
-EXCEPTION(Windows_SEH_DatatypeMisalignment)
-EXCEPTION(Windows_SEH_GuardPage)
-EXCEPTION(Windows_SEH_DivideByZero)
-EXCEPTION(Windows_SEH_InvalidHandle)
-EXCEPTION(Windows_SEH_StackOverflow)
-
 static void SEHTranslator(unsigned int u, EXCEPTION_POINTERS* /*pExp*/)
 {
+    _asm int 3;
     switch (u)
     {
     case EXCEPTION_ACCESS_VIOLATION:
-        THROW(OpenHome::Windows_SEH_AccessViolation);
+        THROW(OpenHome::AssertionFailed);
     case EXCEPTION_ARRAY_BOUNDS_EXCEEDED:
-        THROW(OpenHome::Windows_SEH_ArrayBoundsExceeded);
+        THROW(OpenHome::AssertionFailed);
     case EXCEPTION_DATATYPE_MISALIGNMENT:
-        THROW(OpenHome::Windows_SEH_DatatypeMisalignment);
+        THROW(OpenHome::AssertionFailed);
     case EXCEPTION_GUARD_PAGE:
-        THROW(OpenHome::Windows_SEH_GuardPage);
+        THROW(OpenHome::AssertionFailed);
     case EXCEPTION_INT_DIVIDE_BY_ZERO:
-        THROW(OpenHome::Windows_SEH_DivideByZero);
+        THROW(OpenHome::AssertionFailed);
     case EXCEPTION_INVALID_HANDLE:
-        THROW(OpenHome::Windows_SEH_InvalidHandle);
+        THROW(OpenHome::AssertionFailed);
     case EXCEPTION_STACK_OVERFLOW:
-        THROW(OpenHome::Windows_SEH_StackOverflow);
+        THROW(OpenHome::AssertionFailed);
     default:
-        THROW(OpenHome::Windows_SEH);
+        THROW(OpenHome::AssertionFailed);
     }
 }
 
