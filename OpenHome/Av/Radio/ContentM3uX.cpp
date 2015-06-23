2096d87b634973edaf85e2b382011640a5bd4260
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2015-06-23 12:15:08 +0100

    Update ContentM3uX to support relative URIs for variant playlists.

--- OpenHome/Av/Radio/ContentM3uX.cpp
+++ OpenHome/Av/Radio/ContentM3uX.cpp
@@ -42,6 +42,7 @@ namespace Av {
 class ContentM3uX : public Media::ContentProcessor
 {
     static const TUint kMaxLineBytes = 2 * 1024;
+    static const Brn kSchemeHttp;
 public:
     ContentM3uX();
     ~ContentM3uX();
@@ -50,11 +51,12 @@ private: // from ContentProcessor
     void Reset() override;
     Media::ProtocolStreamResult Stream(IReader& aReader, TUint64 aTotalBytes) override;
 private:
+    ReaderUntil* iReaderUntil;
+    Uri iUriPlaylist;
+    Uri iUriHls;
     TUint iBandwidth;
     TBool iIsAudio;
     TBool iCacheNextUri;
-    Bws<Uri::kMaxUriBytes> iUriHls;
-    ReaderUntil* iReaderUntil;
 };
 
 } // namespace Av
@@ -73,6 +75,8 @@ ContentProcessor* ContentProcessorFactory::NewM3uX()
 
 // ContentM3uX
 
+const Brn ContentM3uX::kSchemeHttp("http");
+
 ContentM3uX::ContentM3uX()
 {
     iReaderUntil = new ReaderUntilS<kMaxLineBytes>(*this);
@@ -83,14 +87,21 @@ ContentM3uX::~ContentM3uX()
     delete iReaderUntil;
 }
 
-TBool ContentM3uX::Recognise(const Brx& /*aUri*/, const Brx& aMimeType, const Brx& aData)
+TBool ContentM3uX::Recognise(const Brx& aUri, const Brx& aMimeType, const Brx& aData)
 {
+    try {
+        iUriPlaylist.Replace(aUri);
+    }
+    catch (UriError&) {
+        return false;
+    }
+
     if (Ascii::CaseInsensitiveEquals(aMimeType, Brn("application/x-mpegurl")) ||
         Ascii::CaseInsensitiveEquals(aMimeType, Brn("application/vnd.apple.mpegurl"))/* ||
         Ascii::CaseInsensitiveEquals(aMimeType, Brn("audio/x-mpegurl")) ||
         Ascii::CaseInsensitiveEquals(aMimeType, Brn("audio/mpegurl"))*/) {
-            // Comparing against audio/x-mpegurl or audio/mpegurl alone could
-            // clash with other M3U files.
+        // Comparing against audio/x-mpegurl or audio/mpegurl alone could
+        // clash with other M3U files.
         return true;
     }
     if (Ascii::Contains(aData, Brn("#EXTM3U")) &&
@@ -104,15 +115,13 @@ void ContentM3uX::Reset()
 {
     iReaderUntil->ReadFlush();
     ContentProcessor::Reset();
+    iUriPlaylist.Clear();
+    iUriHls.Clear();
     iBandwidth = 0;
     iIsAudio = false;
     iCacheNextUri = false;
-    iUriHls.SetBytes(0);
 }
 
-// FIXME - need to iterate over to find highest quality stream first
-// Currently, TuneIn only provides one master playlist per stream quality (and we
-// receive streams ordered by quality).
 ProtocolStreamResult ContentM3uX::Stream(IReader& aReader, TUint64 aTotalBytes)
 {
     LOG(kMedia, "ContentM3uX::Stream\n");
@@ -122,18 +131,18 @@ ProtocolStreamResult ContentM3uX::Stream(IReader& aReader, TUint64 aTotalBytes)
     try {
         for (;;) {
             Brn line = ReadLine(*iReaderUntil, bytesRemaining);
-            if (line.Bytes() == 0) {// || line.BeginsWith(Brn("#"))) {
+            if (line.Bytes() == 0) {
                 continue; // empty/comment line
             }
 
             // Only want to stream one variant, but one or more may fail.
             // If that is the case, definitely want to fall through to other variants.
-            Parser p(line);
-            Brn prefix = p.Next(':');
 
-            if (prefix == Brn("#EXT-X-STREAM-INF")) {
+            if (line.BeginsWith(Brn("#EXT-X-STREAM-INF"))) {
                 TUint attribBandwidth = 0;
                 TBool attribAudio = false;
+                Parser p(line);
+                (void)p.Next(':');  // Discard "#EXT-X-STREAM-INF:".
                 while (!p.Finished()) {
                     Brn attrib = p.Next(',');
                     Parser attribParser(attrib);
@@ -169,10 +178,56 @@ ProtocolStreamResult ContentM3uX::Stream(IReader& aReader, TUint64 aTotalBytes)
                     iBandwidth = attribBandwidth;
                 }
             }
-            else if (iCacheNextUri && (prefix == Brn("http") || prefix == Brn("HTTP"))) {
-                iUriHls.Replace("hls");
-                TUint offset = prefix.Bytes();
-                iUriHls.Append(line.Ptr()+offset, line.Bytes()-offset);
+            else if (iCacheNextUri) {
+                // Check to see if URL is absolute.
+                TBool absolute = false;
+                TBool supportedScheme = true;
+                try {
+                    Uri uri;
+                    uri.Replace(line);
+                    absolute = true;
+                    if (!Ascii::CaseInsensitiveEquals(uri.Scheme(), kSchemeHttp)) { // Only support HTTP.
+                        supportedScheme = false;
+                    }
+                }
+                catch (UriError&) {
+                    // Not an absolute URL.
+                }
+
+                if (supportedScheme) {
+                    // Currently only support HTTP (not HTTPS).
+                    // Fail here if non-HTTP stream discovered as, due to custom
+                    // URI that is sent, it would be impossible to easily detect in
+                    // other components that the fault was an unsupported URI.
+                    try {
+                        Bws<Uri::kMaxUriBytes> uri("hls");
+                        if (absolute) {
+                            const TUint offset = kSchemeHttp.Bytes();
+                            uri.Append(line.Ptr()+offset, line.Bytes()-offset);
+                            iUriHls.Replace(uri);
+                        }
+                        else {
+                            if (Ascii::CaseInsensitiveEquals(iUriPlaylist.Scheme(), kSchemeHttp)) { // Playlist URI should have "http" prefix.
+                                // Uri::Replace(aBaseUri, aRelativeUri) expects aBaseUri to have been stripped, so do that here.
+                                const TUint offset = iUriPlaylist.Scheme().Bytes();
+                                const Brn tail(iUriPlaylist.AbsoluteUri().Ptr()+offset, iUriPlaylist.AbsoluteUri().Bytes()-offset);
+                                //uri.Append("://");
+                                Parser p(tail);
+                                while (!p.Finished()) {
+                                    const Brn next = p.Next('/');
+                                    if (!p.Finished()) {
+                                        uri.Append(next);
+                                        uri.Append('/');
+                                    }
+                                }
+                                iUriHls.Replace(uri, line);
+                            }
+                        }
+                    }
+                    catch (UriError&) {
+                        iUriHls.Clear();
+                    }
+                }
                 iCacheNextUri = false;
             }
         }
@@ -182,21 +237,21 @@ ProtocolStreamResult ContentM3uX::Stream(IReader& aReader, TUint64 aTotalBytes)
 
     // Should get here via a ReaderError.
     // Need to check if entire M3U has been read, or if there was an unexpected break in stream.
-    if (iUriHls.Bytes() == 0 && bytesRemaining == 0 && bytesRemaining < aTotalBytes) {
+    if (iUriHls.AbsoluteUri().Bytes() == 0 && bytesRemaining == 0 && bytesRemaining < aTotalBytes) {
         // Parsed entire file and didn't find stream.
         return EProtocolStreamErrorUnrecoverable;
     }
-    else if (iUriHls.Bytes() == 0 && bytesRemaining > 0 && bytesRemaining < aTotalBytes) {
+    else if (iUriHls.AbsoluteUri().Bytes() == 0 && bytesRemaining > 0 && bytesRemaining < aTotalBytes) {
         // Started parsing, but didn't finish.
         return EProtocolStreamErrorRecoverable;
     }
-    else if (iUriHls.Bytes() == 0) {
+    else if (iUriHls.AbsoluteUri().Bytes() == 0) {
         // Didn't find URL for unknown reason; give up.
         return EProtocolStreamErrorUnrecoverable;
     }
 
     TBool streamSucceeded = false;
-    ProtocolStreamResult res = iProtocolSet->Stream(iUriHls);
+    ProtocolStreamResult res = iProtocolSet->Stream(iUriHls.AbsoluteUri());
     if (res == EProtocolStreamStopped) {
         return EProtocolStreamStopped;
     }
