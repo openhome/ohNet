45dcfdf25ab8160527dbdeb5a1de724729affde9
Simon Chisholm <simon.chisholm@linn.co.uk>
2015-07-23 13:16:07 +0100

    Remove Linn prefixes for udns.  These are needed by older versions of Konfig but prevent Kinsky from recognising devices.

--- OpenHome/Av/Tests/TestMediaPlayerMain.cpp
+++ OpenHome/Av/Tests/TestMediaPlayerMain.cpp
@@ -45,8 +45,8 @@ int CDECL main(int aArgc, char* aArgv[])
 
     // Set/construct UDN.
     Bwh udn;
-    // FIXME - 4c494e4e- prefix to udn is temporary measure to allow Linn Konfig to recognise our devices
-    TestMediaPlayerInit::AppendUniqueId(dvStack->Env(), options.Udn().Value(), Brn("4c494e4e-TestMediaPlayer"), udn);
+    // Note: prefix udn with 4c494e4e- to get older versions of Linn Konfig to recognise our devices
+    TestMediaPlayerInit::AppendUniqueId(dvStack->Env(), options.Udn().Value(), Brn("TestMediaPlayer"), udn);
 
     // Create TestMediaPlayer.
     TestMediaPlayer* tmp = new TestMediaPlayer(*dvStack, udn, options.Room().CString(), options.Name().CString(),
