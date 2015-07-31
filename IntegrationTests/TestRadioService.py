2d6c7bf1ce49ce6f5352717eda53ae70b6daa9d0
grahamdo <graham.douglas@linn.co.uk>
2015-07-31 14:23:21 +0100

    Update Radio service test to use local product specific constants

--- IntegrationTests/TestRadioService.py
+++ IntegrationTests/TestRadioService.py
@@ -23,7 +23,25 @@ class TestRadioService( BASE.CommonRadioService ):
 
     def __init__( self ):
         BASE.CommonRadioService.__init__( self )
-        self.doc = __doc__
+        self.doc          = __doc__
+        self.tuneInBrowse = 'Browse.ashx?c=presets&formats=aac,ogg,hls'
+        self.protocolInfo = 'http-get:*:audio/x-flac:*,'       +\
+                            'http-get:*:audio/wav:*,'          +\
+                            'http-get:*:audio/wave:*,'         +\
+                            'http-get:*:audio/x-wav:*,'        +\
+                            'http-get:*:audio/aiff:*,'         +\
+                            'http-get:*:audio/x-aiff:*,'       +\
+                            'http-get:*:audio/x-m4a:*,'        +\
+                            'http-get:*:audio/x-scpls:*,'      +\
+                            'http-get:*:text/xml:*,'           +\
+                            'http-get:*:audio/aac:*,'          +\
+                            'http-get:*:audio/aacp:*,'         +\
+                            'http-get:*:audio/mp4:*,'          +\
+                            'http-get:*:audio/ogg:*,'          +\
+                            'http-get:*:audio/x-ogg:*,'        +\
+                            'http-get:*:application/ogg:*,'    +\
+                            'tidalhifi.com:*:*:*,'             +\
+                            'qobuz.com:*:*:*'
 
 
 if __name__ == '__main__':
