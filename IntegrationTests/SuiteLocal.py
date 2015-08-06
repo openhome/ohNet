6f4d76548fee13fea2b168443fe9c2222a2108ca
Simon Chisholm <simon.chisholm@linn.co.uk>
2015-08-06 18:09:12 +0100

    Fix typo in test list

--- IntegrationTests/SuiteLocal.py
+++ IntegrationTests/SuiteLocal.py
@@ -120,10 +120,10 @@ tests = [
 
     # Misc
     [ 'TestAudioConx',                'local', 'all'                                                                                 ],
-    [ 'TestStandbyCycleSoak',         'local',  100                                                                                  ]
+    [ 'TestStandbyCycleSoak',         'local',  100                                                                                  ],
 
     ### moved due to instability
-    [ 'TestAvTransportPlayTracks',    'local', 'none',  server,  plMix,   8,           1,           'false'                          ],
+    [ 'TestAvTransportPlayTracks',    'local', 'none',  server,  plMix,   8,           1,           'false'                          ]
 
     # N/A to SoftPlayer(s)
     #    - AutoPlay
