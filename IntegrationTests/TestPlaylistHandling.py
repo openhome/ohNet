7760bb6f18f51d33e53f0303dc1cf1c708a75621
grahamdo <graham.douglas@linn.co.uk>
2015-08-18 13:20:55 +0100

    UPdate protocolInfo check tables

--- IntegrationTests/TestPlaylistHandling.py
+++ IntegrationTests/TestPlaylistHandling.py
@@ -28,22 +28,30 @@ class TestPlaylistHandling( BASE.CommonPlaylistHandling ):
     def __init__( self ):
         BASE.CommonPlaylistHandling.__init__( self )
         self.doc          = __doc__
-        self.protocolInfo = 'http-get:*:audio/x-flac:*,'    +\
-                            'http-get:*:audio/wav:*,'       +\
-                            'http-get:*:audio/wave:*,'      +\
-                            'http-get:*:audio/x-wav:*,'     +\
-                            'http-get:*:audio/aiff:*,'      +\
-                            'http-get:*:audio/x-aiff:*,'    +\
-                            'http-get:*:audio/x-m4a:*,'     +\
-                            'http-get:*:audio/x-scpls:*,'   +\
-                            'http-get:*:text/xml:*,'        +\
-                            'http-get:*:audio/aac:*,'       +\
-                            'http-get:*:audio/aacp:*,'      +\
-                            'http-get:*:audio/mp4:*,'       +\
-                            'http-get:*:audio/ogg:*,'       +\
-                            'http-get:*:audio/x-ogg:*,'     +\
-                            'http-get:*:application/ogg:*,' +\
-                            'tidalhifi.com:*:*:*,'          +\
+        self.protocolInfo = 'http-get:*:audio/mp4:*,'                       +\
+                            'http-get:*:application/vnd.apple.mpegurl:*,'   +\
+                            'http-get:*:audio/x-flac:*,'                    +\
+                            'http-get:*:audio/wav:*,'                       +\
+                            'http-get:*:audio/wave:*,'                      +\
+                            'http-get:*:audio/x-wav:*,'                     +\
+                            'http-get:*:audio/aiff:*,'                      +\
+                            'http-get:*:audio/x-aiff:*,'                    +\
+                            'http-get:*:audio/aifc:*,'                      +\
+                            'http-get:*:audio/x-aifc:*,'                    +\
+                            'http-get:*:audio/aac:*,'                       +\
+                            'http-get:*:audio/aacp:*,'                      +\
+                            'http-get:*:audio/x-m4a:*,'                     +\
+                            'http-get:*:audio/mpeg:*,'                      +\
+                            'http-get:*:audio/x-mpeg:*,'                    +\
+                            'http-get:*:audio/mp1:*,'                       +\
+                            'http-get:*:audio/ogg:*,'                       +\
+                            'http-get:*:audio/x-ogg:*,'                     +\
+                            'http-get:*:application/ogg:*,'                 +\
+                            'http-get:*:audio/x-mpegurl:*,'                 +\
+                            'http-get:*:audio/mpegurl:*,'                   +\
+                            'http-get:*:audio/x-scpls:*,'                   +\
+                            'http-get:*:text/xml:*,'                        +\
+                            'tidalhifi.com:*:*:*,'                          +\
                             'qobuz.com:*:*:*'
 
 
