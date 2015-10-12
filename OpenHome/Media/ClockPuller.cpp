02292b0dcf42bc4129e4cfb5868502d6e1ac4c62
Simon Chisholm <simon.chisholm@linn.co.uk>
2015-10-12 13:15:24 +0100

    Allow small negative clock pulls

--- OpenHome/Media/ClockPuller.cpp
+++ OpenHome/Media/ClockPuller.cpp
@@ -16,7 +16,7 @@ void ClockPullerUtils::PullClock(TUint& aMultiplier, TInt aDriftJiffies, TUint64
     TInt64 driftJiffies = aDriftJiffies;
     TInt64 update = -driftJiffies * aMultiplier;
     update /= (TInt64)aPeriodJiffies;
-    if (update > kMaxPull || update < kMaxPull) {
+    if (update > kMaxPull || update < -kMaxPull) {
         LOG(kMedia, "Rejected pull of %llx (%d%%)\n", update, static_cast<TInt>((100*update)/aMultiplier));
     }
     else {
