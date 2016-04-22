ff53ac6bd1e15102f190bbc0630a3f06da54c8f6
Simon Chisholm <simon.chisholm@linn.co.uk>
2016-04-22 13:40:48 +0100

    Log jiffy delays as decimal

--- OpenHome/Media/Pipeline/Logger.cpp
+++ OpenHome/Media/Pipeline/Logger.cpp
@@ -119,7 +119,7 @@ Msg* Logger::ProcessMsg(MsgDelay* aMsg)
     if (IsEnabled(EMsgDelay)) {
         const TUint total = aMsg->DelayJiffies();
         const TUint animator = aMsg->AnimatorDelayJiffies();
-        Log::Print("Pipeline (%s): delay {total: %ums (%x jiffies), animator: %ums (%x jiffies)}\n",
+        Log::Print("Pipeline (%s): delay {total: %ums (%u jiffies), animator: %ums (%u jiffies)}\n",
                    iId, total/Jiffies::kPerMs, total, animator/Jiffies::kPerMs, animator);
     }
     return aMsg;
