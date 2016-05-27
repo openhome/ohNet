fe5ca134ddaa2a50ed936d15d91efa4ab1009aa2
Simon Chisholm <simon.chisholm@linn.co.uk>
2016-05-27 14:40:54 +0100

    Fix typo in comment

--- OpenHome/Av/Songcast/OhmMsg.cpp
+++ OpenHome/Av/Songcast/OhmMsg.cpp
@@ -332,7 +332,7 @@ void OhmMsgAudio::SetResent(TBool aValue)
 {
     iResent = aValue;
     const TUint flagsIndex = iStreamHeaderOffset + 8 + 1; // +8 for Ohm header, +1 to skip audio header length
-    ASSERT((iUnifiedBuffer[flagsIndex] & 0xE8) == 0); // check that kFlagResent and unused bits aren't set (implying weflagsIndex is wrong)
+    ASSERT((iUnifiedBuffer[flagsIndex] & 0xE8) == 0); // check that kFlagResent and unused bits aren't set (implying flagsIndex is wrong)
     iUnifiedBuffer[flagsIndex] |= kFlagResent;
 }
 
