79a30a5e86f7c3aae7b74ac9e2cce5f6c5085bfb
Simon Chisholm <simon.chisholm@linn.co.uk>
2016-07-15 14:06:36 +0100

    Adjust songcast sender thread priority to move below the (new) delay thread.

--- OpenHome/Av/Songcast/SourceReceiver.cpp
+++ OpenHome/Av/Songcast/SourceReceiver.cpp
@@ -473,7 +473,7 @@ SongcastSender::SongcastSender(IMediaPlayer& aMediaPlayer, ZoneHandler& aZoneHan
     iLoggerSender = new Logger("Sender", *iSender);
     //iLoggerSender->SetEnabled(true);
     //iLoggerSender->SetFilter(Logger::EMsgAll);
-    iSenderThread = new SenderThread(*iLoggerSender, priorityMax-2);
+    iSenderThread = new SenderThread(*iLoggerSender, priorityMax-3);
     iSplitter = new Splitter(*iSenderThread, aMode);
     iLoggerSplitter = new Logger(*iSplitter, "Splitter");
     iSplitter->SetUpstream(pipeline.InsertElements(*iLoggerSplitter));
