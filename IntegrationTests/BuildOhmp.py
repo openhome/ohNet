66a3368091f7025b8e8ce870b151f0169296de38
grahamdo <graham.douglas@linn.co.uk>
2016-08-04 13:21:31 +0100

    Revert last change - now implemented in base class

--- IntegrationTests/BuildOhmp.py
+++ IntegrationTests/BuildOhmp.py
@@ -4,7 +4,6 @@
 import _Paths
 import CommonBuild as BASE
 import os
-import platform
 import sys
 
 # NOTE - this requires GIT to operate without requirement for password entry.
@@ -74,50 +73,8 @@ class BuildOhmp( BASE.CommonBuild ):
         self._Execute( cleanDeps, ohmpPath, 'Cleaning dependencies' )
         self._Execute( buildCmd, ohmpPath, 'Executing build' )
 
-    @staticmethod
-    def UpdatePyOhNetPath():
-        """Update python search path (as PyOhNet may have changed from Release to Debug within build"""
-        # Get pathing for for FunctionalTest
-        root = os.path.dirname( __file__ )
-        while not os.path.isdir( os.path.join( root, 'IntegrationTests' ) ):
-            root = os.path.dirname( root )
-        deps = os.path.join( root, 'dependencies' )
-
-        # Get pathing for PyOhNet (platform/architecture dependent)
-        ohnetRls = ''
-        ohnetDbg = ''
-        _platform = platform.system( )
-        _architecture = platform.architecture( )
-        if _platform in ['Windows', 'cli']:
-            if '32' in _architecture[0]:
-                ohnetRls = os.path.abspath( os.path.join( deps, 'Windows-x86/ohNet-Windows-x86-Release' ) )
-                ohnetDbg = os.path.abspath( os.path.join( deps, 'Windows-x86/ohNet-Windows-x86-Debug' ) )
-            elif '64' in _architecture[0]:
-                ohnetRls = os.path.abspath( os.path.join( deps, 'Windows-x64/ohNet-Windows-x64-Release' ) )
-                ohnetDbg = os.path.abspath( os.path.join( deps, 'Windows-x64/ohNet-Windows-x64-Debug' ) )
-        elif _platform == 'Linux':
-            if '32' in _architecture[0]:
-                ohnetRls = os.path.abspath( os.path.join( deps, 'Linux-x86/ohNet-Linux-x86-Release' ) )
-                ohnetDbg = os.path.abspath( os.path.join( deps, 'Linux-x86/ohNet-Linux-x86-Debug' ) )
-            elif '64' in _architecture[0]:
-                ohnetRls = os.path.abspath( os.path.join( deps, 'Linux-x64/ohNet-Linux-x64-Release' ) )
-                ohnetDbg = os.path.abspath( os.path.join( deps, 'Linux-x64/ohNet-Linux-x64-Debug' ) )
-        elif _platform == 'Darwin':
-            ohnetRls = os.path.abspath( os.path.join( deps, 'Mac-x64/ohNet-Mac-x64-Release' ) )
-            ohnetDbg = os.path.abspath( os.path.join( deps, 'Mac-x64/ohNet-Mac-x64-Debug' ) )
-        if os.path.exists( ohnetRls ):
-            ohnet = ohnetRls
-        elif os.path.exists( ohnetDbg ):
-            ohnet = ohnetDbg
-        else:
-            ohnet = deps  # should never happen
-
-        # configure the Python module search path
-        sys.path.insert( 0, os.path.normpath( os.path.join( ohnet, 'lib' ) ) )
-
     def Cleanup( self ):
         """Clean up base class"""
-        self.UpdatePyOhNetPath()
         BASE.CommonBuild.Cleanup( self )
 
 
