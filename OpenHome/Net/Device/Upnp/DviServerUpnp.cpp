f0ac7a943707f85e0bee6c799c8bce7a8763cde4
Tim Cussins <tim.cussins@linn.co.uk>
2016-08-26 13:53:07 +0100

    Revert "Initial events are occasionally failing to be delivered - the tcp connect attempt from the device stack is never acknowledged by the network stack on the control point.  This can cause control point's discovery to fail.  Mitigate this by retrying the connect for initial events.  See #4469."
    This reverts commit 1d7bd01f1ddaeed4410a1cb3e242e26e92b90f11.
    
    These retries have caused big slowdown during boot, in the presence of Windows 7 machines.
    According to GDo, they Sub and Unsub quickly, and our initial events all fail to connect.

--- OpenHome/Net/Device/Upnp/DviServerUpnp.cpp
+++ OpenHome/Net/Device/Upnp/DviServerUpnp.cpp
@@ -336,31 +336,9 @@ void PropertyWriterUpnp::Connect()
     iSubscriber.AppendAddress(buf);
     Log::Print("PropertyWriterUpnp connecting to %s\n", buf.Ptr());
 #endif
-    static const TUint kInitialEventConnectRetries = 3;
-    TUint retries = (iSequenceNumber == 0? kInitialEventConnectRetries : 0);
-    for (;;) {
-        try {
-            iSocket.Open(iEnv);
-            iSocket.Connect(iSubscriber, iEnv.InitParams()->TcpConnectTimeoutMs());
-            //iSocket.LogVerbose(true);
-            return;
-        }
-        catch (NetworkTimeout&) {
-            if (retries == 0) {
-                throw;
-            }
-        }
-        catch (NetworkError&) {
-            if (retries == 0) {
-                throw;
-            }
-        }
-        --retries;
-        try {
-            iSocket.Close();
-        }
-        catch (NetworkError&) {}
-    }
+    iSocket.Open(iEnv);
+    iSocket.Connect(iSubscriber, iEnv.InitParams()->TcpConnectTimeoutMs());
+    //iSocket.LogVerbose(true);
 }
 
 void PropertyWriterUpnp::WriteHeaders(TUint aContentLength)
