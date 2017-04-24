fa4b4daf498da0179fdad75bcb0ae6c0598d9314
Simon Chisholm <simon.chisholm@linn.co.uk>
2017-04-24 13:01:12 +0100

    Ignore failures to read network time if we have a previous reading to work from and our local millisecond timer hasn't wrapped (so, the last good reading was in the last 7 weeks). Should mitigate problems playing tracks from Qobuz.

--- OpenHome/UnixTimestamp.cpp
+++ OpenHome/UnixTimestamp.cpp
@@ -5,16 +5,20 @@
 #include <OpenHome/Private/Timer.h>
 #include <OpenHome/NtpClient.h>
 
+#include <algorithm>
 #include <limits.h>
 
 using namespace OpenHome;
 
+const TUint UnixTimestamp::kSecsBetweenNtpAndUnixEpoch = 2208988800; // secs between 1900 and 1970
+const TUint UnixTimestamp::kTimeResyncFreqMs = 24 * 60 * 60 * 1000; // 1 day
 
 UnixTimestamp::UnixTimestamp(Environment& aEnv)
     : iEnv(aEnv)
     , iLock("UNTS")
     , iNtpClient(aEnv)
     , iTimestampValid(false)
+    , iTimestampRenew(true)
     , iStartMs(UINT_MAX)
 {
     iTimer = new Timer(aEnv, MakeFunctor(*this, &UnixTimestamp::TimestampExpired), "UnixTimestamp");
@@ -27,9 +31,8 @@ UnixTimestamp::~UnixTimestamp()
 
 void UnixTimestamp::Reset()
 {
-    iLock.Wait();
-    iTimestampValid = false;
-    iLock.Signal();
+    AutoMutex _(iLock);
+    iTimestampRenew = true;
 }
 
 TUint UnixTimestamp::Now()
@@ -38,18 +41,23 @@ TUint UnixTimestamp::Now()
     if (Time::Now(iEnv) < iStartMs) {
         iTimestampValid = false;
     }
-    if (!iTimestampValid) {
+    if (!iTimestampValid || iTimestampRenew) {
         NtpTimestamp networkTime;
         TUint networkDelayMs;
         if (!iNtpClient.TryGetNetworkTime(networkTime, networkDelayMs)) {
-            THROW(UnixTimestampUnavailable);
+            if (!iTimestampValid) {
+                THROW(UnixTimestampUnavailable);
+            }
         }
         // round up network delay to make up for rounding down fractional component of networkTime
         iStartSeconds = networkTime.Seconds() - kSecsBetweenNtpAndUnixEpoch + ((networkDelayMs + 500) / 1000);
         iStartMs = Time::Now(iEnv);
         // recalculate when local millisecond timer wraps
-        iTimer->FireIn(UINT_MAX - iStartMs + 1);
+        const TUint timeTillWrap = std::max(static_cast<TUint>(UINT_MAX) - iStartMs, 1u);
+        const TUint timerDuration = std::min(kTimeResyncFreqMs, timeTillWrap);
+        iTimer->FireIn(timerDuration);
         iTimestampValid = true;
+        iTimestampRenew = false;
     }
 
     const TUint unixTs = iStartSeconds + ((Time::Now(iEnv) - iStartMs) / 1000);
