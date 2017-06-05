47c83f0464f7ca91883af3e3bca78550e38467d5
grahamdo <graham.douglas@linn.co.uk>
2017-06-05 15:52:32 +0100

    Add Transport service test

---
+++ IntegrationTests/_ProductData.py
@@ -0,0 +1,12 @@
+"""_ProductData - product specific data
+"""
+
+# Available source specified by type
+ds =   {'NetAux'  : 'Net Aux',
+        'Playlist': 'Playlist',
+        'Radio'   : 'Radio',
+        'Receiver': 'Songcast',
+        'Spotify' : 'Spotify',
+        'UpnpAv'  : 'UPnP AV'}
+
+kSrcByType = { 'SoftPlayer' : ds }
