c788afd8c6ce76fdf0e228ccf984d31ee29f849d
Simon Chisholm <simon.chisholm@linn.co.uk>
2017-06-28 12:15:02 +0100

    Enable logging of NTP responses to help track down the cause of a bug report that isn't easily reproducible.

--- OpenHome/NtpClient.cpp
+++ OpenHome/NtpClient.cpp
@@ -24,7 +24,7 @@ NtpClient::NtpClient(Environment& aEnv)
     , iUdpReader(iSocket)
     , iReadBuffer(iUdpReader)
     , iNextServerIndex(0)
-    , iLogEnable(false)
+    , iLogEnable(true) // temporarily enabled to help diagnose http://forums.linn.co.uk/bb/showthread.php?tid=36476&pid=440782#pid440782
 {
     iWriteBuffer = new Sws<kFrameBytes>(*this);
     iReadTimeout = new Timer(aEnv, MakeFunctor(*this, &NtpClient::ReadTimeout), "NtpClient");
