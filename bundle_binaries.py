cd2e0d5d359b9d4421ceb4b58b812faeeee398ff
Simon Chisholm <simon.chisholm@linn.co.uk>
2017-11-15 15:22:00 +0000

    Allow bundling of iOs-x64 builds

--- bundle_binaries.py
+++ bundle_binaries.py
@@ -82,10 +82,12 @@ ALL_TARGETS = {
         BuildTarget("FreeBSD", "x86",   "Release"): BuildInfo(builddir="Build/Obj/Posix/Release"),
 
         BuildTarget("iOs",     "x86",   "Debug"):   BuildInfo(builddir="Build/Obj/iOs-x86/Debug"),
+        BuildTarget("iOs",     "x64",   "Debug"):   BuildInfo(builddir="Build/Obj/iOs-x64/Debug"),
         BuildTarget("iOs",     "armv6", "Debug"):   BuildInfo(builddir="Build/Obj/iOs-armv6/Debug"),
         BuildTarget("iOs",     "armv7", "Debug"):   BuildInfo(builddir="Build/Obj/iOs-armv7/Debug"),
         BuildTarget("iOs",     "arm64", "Debug"):   BuildInfo(builddir="Build/Obj/iOs-arm64/Debug"),
         BuildTarget("iOs",     "x86",   "Release"): BuildInfo(builddir="Build/Obj/iOs-x86/Release"),
+        BuildTarget("iOs",     "x64",   "Release"): BuildInfo(builddir="Build/Obj/iOs-x64/Release"),
         BuildTarget("iOs",     "armv6", "Release"): BuildInfo(builddir="Build/Obj/iOs-armv6/Release"),
         BuildTarget("iOs",     "armv7", "Release"): BuildInfo(builddir="Build/Obj/iOs-armv7/Release"),
         BuildTarget("iOs",     "arm64", "Release"): BuildInfo(builddir="Build/Obj/iOs-arm64/Release"),
