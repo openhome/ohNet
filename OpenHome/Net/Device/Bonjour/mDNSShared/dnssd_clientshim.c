79bfb891e03a0ed6f3efd1b37d65cdb7c1422d2f
joshh <josh.hahn@linn.co.uk>
2018-02-22 10:21:55 +0000

    shot in the dark for android fix

--- OpenHome/Net/Device/Bonjour/mDNSShared/dnssd_clientshim.c
+++ OpenHome/Net/Device/Bonjour/mDNSShared/dnssd_clientshim.c
@@ -492,10 +492,10 @@ mDNSlocal void FoundServiceInfo(mDNS *const m, DNSQuestion *question, const Reso
                          fullname,
                          targethost,
                          port,
-                         x->Av4->rdata->u.ipv4.b,
+                         (unsigned char*)x->Av4->rdata->u.ipv4.b,
                          type,
                          x->TXT->rdlength,
-                         x->TXT->rdata->u.txt.c,
+                         (unsigned char*)x->TXT->rdata->u.txt.c,
                          x->context);
         }
     }
