d48949339e1de091da92781195e401f6ae52c94c
joshh <josh.hahn@linn.co.uk>
2018-03-13 11:51:35 +0000

    shot in the dark

--- OpenHome/Net/Bindings/Java/InitParams.c
+++ OpenHome/Net/Bindings/Java/InitParams.c
@@ -666,9 +666,10 @@ JNIEXPORT void JNICALL Java_org_openhome_net_core_InitParams_OhNetInitParamsSetD
 	OhNetHandleInitParams params = (OhNetHandleInitParams) (size_t)aParams;
     const char* hostName = (*aEnv)->GetStringUTFChars(aEnv, aHostName, NULL);
     aClass = aClass;
-    uint8_t reqCache = (uint8_t)aRequiresMdnsCache;
+    aRequiresMdnsCache = aRequiresMdnsCache;
+    //uint8_t reqCache = (uint8_t)aRequiresMdnsCache;
 
-    OhNetInitParamsSetDvEnableBonjour(params, hostName, reqCache);
+    OhNetInitParamsSetDvEnableBonjour(params, hostName, 0);
     (*aEnv)->ReleaseStringUTFChars(aEnv, aHostName, hostName);
 }
 
