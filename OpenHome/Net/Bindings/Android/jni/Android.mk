f4c78ae6b516c710b84dd25218861605dca7ea2f
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2019-03-07 15:47:05 +0000

    Change CFLAGS to be compatible with newer Android NDK.

--- OpenHome/Net/Bindings/Android/jni/Android.mk
+++ OpenHome/Net/Bindings/Android/jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 ohroot := ../../../../../
 
 LOCAL_C_INCLUDES := $(ohroot)Build/Include
-LOCAL_CFLAGS := -fexceptions -Wall -Werror -pipe -D_GNU_SOURCE -D_REENTRANT -DDEFINE_LITTLE_ENDIAN -DDEFINE_TRACE -fvisibility=hidden -Wno-psabi -Wno-unused-but-set-variable
+LOCAL_CFLAGS := -fexceptions -Wall -Werror -pipe -D_GNU_SOURCE -D_REENTRANT -DDEFINE_LITTLE_ENDIAN -DDEFINE_TRACE -fvisibility=hidden -Wno-unused
 LOCAL_LDLIBS := -llog
 LOCAL_MODULE    := ohNet
 LOCAL_SRC_FILES := $(ohroot)OpenHome/Ascii.cpp \
