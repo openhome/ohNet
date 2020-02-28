e7ee76022b02bfc6483f5e7e4837889b57b22435
Lewis Ross <lewis.ross@linn.co.uk>
2020-02-28 10:02:32 +0000

    #ifndef DEFINE_WINDOWS_UNIVERSAL for mNDSCoreReceive in MdnsPlatform for winrt builds

--- OpenHome/Net/Device/Bonjour/MdnsPlatform.cpp
+++ OpenHome/Net/Device/Bonjour/MdnsPlatform.cpp
@@ -739,9 +739,10 @@ void MdnsPlatform::ReceiveMulticastPacket(const Brx& aMsg, const Endpoint aSrc, 
     }
 
     if (interfaceId != (mDNSInterfaceID)0) {
+#ifndef DEFINE_WINDOWS_UNIVERSAL
         AutoMutex amx(iMulticastReceiveLock);
-        DNSMessage* msg = reinterpret_cast<DNSMessage*>(ptr);
-        mDNSCoreReceive(iMdns, msg, ptr + bytes, &src, srcport, &dst, dstport, interfaceId);
+        mDNSCoreReceive(iMdns, ptr, ptr + bytes, &src, srcport, &dst, dstport, interfaceId);
+#endif // DEFINE_WINDOWS_UNIVERSAL
     }
 }
 
