4dd5130bc576b42223a813745da4bb091ba37e64
Bryan T <blondejamtart@gmail.com>
2024-05-28 10:39:41 +0100

    comments

--- yocto_build.sh
+++ yocto_build.sh
@@ -5,6 +5,9 @@ else
     YOCTO_SDK_ENV=$1
 fi
 source $YOCTO_SDK_ENV
+# Yocto env appends essential build flags to CC/CXX environment variables;
+# our makefiles doesn't use CC to determine compiler since we do some more complex stuff to deal with building under other OSes.
+# just strip out these flags and place in the CFLAGS variable since this is still used
 _CC=$(python -c 'import os; print(" ".join(os.getenv("CC").split(" ")[1:]))')
 _CXX=$(python -c 'import os; print(" ".join(os.getenv("CXX").split(" ")[1:]))')
 make clean 
